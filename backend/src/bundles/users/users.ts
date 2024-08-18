1beb23aa7b49aae41b8e826104fa13087027af27
JKaypa <jose_kaypa@hotmail.com>
2024-08-18 16:09:59 -0500

    OV-2: + import sign in dto types

--- backend/src/bundles/users/users.ts
+++ backend/src/bundles/users/users.ts
@@ -11,6 +11,8 @@ const userController = new UserController(logger, userService);
 
 export { userController, userService };
 export {
+    type UserSignInRequestDto,
+    type UserSignInResponseDto,
     type UserSignUpRequestDto,
     type UserSignUpResponseDto,
 } from './types/types.js';
