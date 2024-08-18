830ec5a097f973a5c5a5625bf365e89289a933c8
JKaypa <jose_kaypa@hotmail.com>
2024-08-18 16:00:09 -0500

    OV-2: * edit service, repository find method type

--- backend/src/common/types/repository.type.ts
+++ backend/src/common/types/repository.type.ts
@@ -1,5 +1,5 @@
 type Repository<T = unknown> = {
-    find(): Promise<T>;
+    find(payload: string | number): Promise<T>;
     findAll(): Promise<T[]>;
     create(payload: unknown): Promise<T>;
     update(): Promise<T>;
