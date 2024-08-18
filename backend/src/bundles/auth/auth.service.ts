483f1c87033dc47188da67ea1d429a9bca4e9a7f
JKaypa <jose_kaypa@hotmail.com>
2024-08-18 16:04:29 -0500

    OV-2: + implement sign in service method

--- backend/src/bundles/auth/auth.service.ts
+++ backend/src/bundles/auth/auth.service.ts
@@ -1,14 +1,50 @@
 import {
+    type UserSignInRequestDto,
+    type UserSignInResponseDto,
     type UserSignUpRequestDto,
     type UserSignUpResponseDto,
 } from '~/bundles/users/types/types.js';
 import { type UserService } from '~/bundles/users/user.service.js';
+import { HttpCode, HttpError } from '~/common/http/http.js';
+import { cryptService } from '~/common/services/services.js';
+
+import { UserValidationMessage } from './enums/enums.js';
 
 class AuthService {
     private userService: UserService;
 
     public constructor(userService: UserService) {
         this.userService = userService;
+    }
+
+    public async signIn(
+        userRequestDto: UserSignInRequestDto,
+    ): Promise<UserSignInResponseDto> {
+        const { email, password } = userRequestDto;
+        const user = await this.userService.find(email);
+
+        if (!user) {
+            throw new HttpError({
+                message: UserValidationMessage.WRONG_CREDENTIALS,
+                status: HttpCode.BAD_REQUEST,
+            });
+        }
+
+        const { passwordHash } = user.toNewObject();
+
+        const isPwdCorrect = cryptService.compareSyncPassword(
+            password,
+            passwordHash,
+        );
+
+        if (!isPwdCorrect) {
+            throw new HttpError({
+                message: UserValidationMessage.WRONG_CREDENTIALS,
+                status: HttpCode.BAD_REQUEST,
+            });
+        }
+
+        return user.toObject();
     }
 
     public signUp(
