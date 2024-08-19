333c400ff561229d2d18b9e2389824a5c02e83c2
Sergiy <sergiy0378@gmail.com>
2024-08-19 21:33:34 +0300

    OV-11: * colors

--- frontend/src/framework/theme/styles/colors.styles.ts
+++ frontend/src/framework/theme/styles/colors.styles.ts
@@ -3,6 +3,12 @@ const colors = {
         900: '#1a365d',
         200: '#b3e0ff',
     },
+    background: {
+        900: '#0a0049',
+        600: '#35399a',
+        300: '#3c9cf5',
+        50: '#e2e1ec',
+    },
     text: {
         default: '#36454f',
         accent: '#ff5733',
