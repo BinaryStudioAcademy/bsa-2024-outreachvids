547891c8f64cb702b382ddc9e43796798bafe35d
Luis Felix <lfelix3011@gmail.com>
2024-08-18 21:04:32 -0400

    OV-4: + save authenticated user after sign-up

--- frontend/src/bundles/auth/store/slice.ts
+++ frontend/src/bundles/auth/store/slice.ts
@@ -2,15 +2,18 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { DataStatus } from '~/bundles/common/enums/enums.js';
 import { type ValueOf } from '~/bundles/common/types/types.js';
+import { type UserSignUpResponseDto } from '~/bundles/users/users.js';
 
 import { signUp } from './actions.js';
 
 type State = {
     dataStatus: ValueOf<typeof DataStatus>;
+    user: UserSignUpResponseDto  | undefined | null;
 };
 
 const initialState: State = {
     dataStatus: DataStatus.IDLE,
+    user: null,
 };
 
 const { reducer, actions, name } = createSlice({
@@ -21,11 +24,15 @@ const { reducer, actions, name } = createSlice({
         builder.addCase(signUp.pending, (state) => {
             state.dataStatus = DataStatus.PENDING;
         });
-        builder.addCase(signUp.fulfilled, (state) => {
+        builder.addCase(signUp.fulfilled, (state, action) => {
+            const payload = action.payload;
+
             state.dataStatus = DataStatus.FULFILLED;
+            state.user = payload;
         });
         builder.addCase(signUp.rejected, (state) => {
             state.dataStatus = DataStatus.REJECTED;
+            state.user = null;
         });
     },
 });
