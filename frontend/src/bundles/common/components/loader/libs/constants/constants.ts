c9b1687c185ffd580f98987867d57eb9e64a112b
Sergiy <sergiy0378@gmail.com>
2024-08-19 14:38:33 +0300

    OV-6: + animation constant

---
+++ frontend/src/bundles/common/components/loader/libs/constants/constants.ts
@@ -0,0 +1,1 @@
+export { spin } from './spin-animation.constant.js';
---
+++ frontend/src/bundles/common/components/loader/libs/constants/spin-animation.constant.ts
@@ -0,0 +1,8 @@
+import { keyframes } from '@chakra-ui/react';
+
+const spin = keyframes`
+    0% { transform: rotate(0deg);}
+    100% { transform: rotate(360deg)}
+`;
+
+export { spin };
