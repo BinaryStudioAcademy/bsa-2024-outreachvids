d3ebf0b248215de77136b9ca3d1ae314aa21405a
Sergiy <sergiy0378@gmail.com>
2024-08-19 14:18:10 +0300

    OV-6: * loader component

--- frontend/src/bundles/common/components/loader/loader.tsx
+++ frontend/src/bundles/common/components/loader/loader.tsx
@@ -1,16 +1,34 @@
-import styles from './styles.module.css';
+import { Box, Circle, Flex, keyframes, Text } from '@chakra-ui/react';
+
+const spin = keyframes`
+    0% { transform: rotate(0deg);}
+    100% { transform: rotate(360deg)}
+`;
 
 const Loader = (): JSX.Element => {
     return (
-        <div className={styles['loader-container']}>
-            <div className={styles['loader']}>
-                <div className={styles['logo-wrapper']}>
-                    <div>LOGO</div>
-                </div>
-                <div className={styles['spinner-overlay']}></div>
-            </div>
-            <h2 className={styles['loader-text']}>Loading...</h2>
-        </div>
+        <Flex flexDirection="column" alignItems="center">
+            <Box position="relative" width="100px" height="100px">
+                <Circle
+                    size="full"
+                    backgroundColor="white"
+                    color="text.default"
+                >
+                    LOGO
+                </Circle>
+                <Circle
+                    position="absolute"
+                    inset="0"
+                    borderWidth="5px"
+                    borderColor="shadow.200"
+                    borderTopColor="brand.secondary.300"
+                    animation={`${spin} 1s linear infinite`}
+                ></Circle>
+            </Box>
+            <Text fontSize="lg" marginTop="10px">
+                Loading...
+            </Text>
+        </Flex>
     );
 };
 
