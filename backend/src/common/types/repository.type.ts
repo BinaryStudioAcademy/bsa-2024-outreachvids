7f48764819d40fd1da6a939ccd2cc331e114a1f1
JKaypa <jose_kaypa@hotmail.com>
2024-08-20 07:06:18 -0500

    OV-2: - remove unnecessary types

--- backend/src/common/types/repository.type.ts
+++ backend/src/common/types/repository.type.ts
@@ -1,7 +1,6 @@
 type Repository<T = unknown> = {
     find(): Promise<T>;
     findAll(): Promise<T[]>;
-    findByEmail(email: string): Promise<T>;
     create(payload: unknown): Promise<T>;
     update(): Promise<T>;
     delete(): Promise<boolean>;
--- backend/src/common/types/service.type.ts
+++ backend/src/common/types/service.type.ts
@@ -1,8 +1,5 @@
 type Service<T = unknown> = {
     find(): Promise<T>;
-    findAll(): Promise<{
-        items: T[];
-    }>;
     findByEmail(email: string): Promise<T>;
     create(payload: unknown): Promise<T>;
     update(): Promise<T>;
