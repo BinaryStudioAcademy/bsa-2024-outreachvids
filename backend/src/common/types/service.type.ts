06fa5d15110343aeeec9684d51529a8bc657fdd1
JKaypa <jose_kaypa@hotmail.com>
2024-08-20 07:21:08 -0500

    OV-2: - * fix service types

--- backend/src/common/types/service.type.ts
+++ backend/src/common/types/service.type.ts
@@ -1,6 +1,8 @@
 type Service<T = unknown> = {
     find(): Promise<T>;
-    findByEmail(email: string): Promise<T>;
+    findAll(): Promise<{
+        items: T[];
+    }>;
     create(payload: unknown): Promise<T>;
     update(): Promise<T>;
     delete(): Promise<boolean>;
