68aba6921f71e6765c1f9a349ec6e8a109aebdb9
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-08-20 08:02:25 -0500

    OV-5: * fix Wrong amount of left-padding spaces

--- backend/src/common/services/token/token.services.ts
+++ backend/src/common/services/token/token.services.ts
@@ -2,20 +2,20 @@ import { type JWTPayload as TokenPayload } from 'jose';
 import { jwtVerify, SignJWT } from 'jose';
 
 class TokenService {
-  private SECRET_KEY: Uint8Array;
-  private EXPIRATION_TIME: string;
+    private SECRET_KEY: Uint8Array;
+    private EXPIRATION_TIME: string;
 
-  public constructor(secretKey: string, expirationTime: string) {
-      this.SECRET_KEY = new TextEncoder().encode(secretKey);
-      this.EXPIRATION_TIME = expirationTime;
-  }
+    public constructor(secretKey: string, expirationTime: string) {
+        this.SECRET_KEY = new TextEncoder().encode(secretKey);
+        this.EXPIRATION_TIME = expirationTime;
+    }
 
 public async createToken(userId: number): Promise<string> {
-      return await new SignJWT({ userId })
-          .setProtectedHeader({ alg: 'HS256' })
-          .setExpirationTime(this.EXPIRATION_TIME)
-          .sign(this.SECRET_KEY);
-  }
+        return await new SignJWT({ userId })
+            .setProtectedHeader({ alg: 'HS256' })
+            .setExpirationTime(this.EXPIRATION_TIME)
+            .sign(this.SECRET_KEY);
+    }
 
 public async verifyToken(token: string): Promise<TokenPayload | null> {
     try {
