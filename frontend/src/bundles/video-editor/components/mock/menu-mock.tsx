3dd913d3b07289f51f360e145293a8e58a021977
Luis Felix <lfelix3011@gmail.com>
2024-08-20 23:08:07 -0400

    OV-29: + mock components for the menu body

---
+++ frontend/src/bundles/video-editor/components/mock/menu-mock.tsx
@@ -0,0 +1,15 @@
+const TemplatesContent: React.FC = () => (
+    <div>This is the Templates content.</div>
+);
+const AvatarsContent: React.FC = () => <div>This is the Avatars content.</div>;
+const ScriptContent: React.FC = () => <div>This is the Script content.</div>;
+const TextContent: React.FC = () => <div>This is the Text content.</div>;
+const AssetsContent: React.FC = () => <div>This is the Assets content.</div>;
+
+export {
+    AssetsContent,
+    AvatarsContent,
+    ScriptContent,
+    TemplatesContent,
+    TextContent,
+};
