5bff40770ce1bc4388e3cff036019a7c76ede0b6
XCODE89 <maph0989@gmail.com>
2024-08-21 10:25:56 -0400

    OV-9: - Unused wildcard route

--- frontend/src/router/routes/public-routes.tsx
+++ frontend/src/router/routes/public-routes.tsx
@@ -1,5 +1,3 @@
-import { Navigate } from 'react-router-dom';
-
 import { Auth } from '~/bundles/auth/pages/auth.js';
 import { AppRoute } from '~/bundles/common/enums/app-route.enum.js';
 
@@ -11,10 +9,6 @@ const publicRoutes = [
     {
         path: AppRoute.SIGN_UP,
         element: <Auth />,
-    },
-    {
-        path: AppRoute.ANY,
-        element: <Navigate to={AppRoute.SIGN_IN} replace />,
     },
 ];
 
