9f17f91d14fd270f9cbcc9b8ca6a1dbcb5d40dc2
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-08-21 16:30:33 +0200

    OV-8: * white routes constant to include path method

--- backend/src/common/constants/white-routes.constants.ts
+++ backend/src/common/constants/white-routes.constants.ts
@@ -1,8 +1,14 @@
 import { ApiPath, AuthApiPath } from 'shared';
 
 const WHITE_ROUTES = [
-    `/api/v1${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`,
-    `/api/v1${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
+    {
+        path: `/api/v1${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`,
+        method: 'POST',
+    },
+    {
+        path: `/api/v1${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
+        method: 'POST',
+    },
 ];
 
 export { WHITE_ROUTES };
