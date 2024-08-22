f0b47b72b78c47c306532b815070d0608a8368e5
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-08-22 19:03:41 +0200

    OV-38: + number of videos indicator

--- frontend/src/bundles/home/components/video-section/video-section.tsx
+++ frontend/src/bundles/home/components/video-section/video-section.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, SimpleGrid } from '@chakra-ui/react';
+import { Box, Flex, Heading, SimpleGrid } from '@chakra-ui/react';
 
 import { VideoCard } from '../video-card/video-card.js';
 
@@ -6,9 +6,25 @@ const VideoSection: React.FC = () => {
     return (
         <>
             <Box padding="17px 28px">
-                <Heading color="typography.900" variant="H3" marginBottom="9px">
-                    Videos
-                </Heading>
+                <Flex alignItems="center" marginBottom="9px">
+                    <Heading
+                        color="typography.900"
+                        variant="H3"
+                        marginRight="11px"
+                    >
+                        Videos
+                    </Heading>
+                    <Box
+                        w="34px"
+                        h="23px"
+                        bg="#D1D4DB"
+                        color="background.600"
+                        textAlign="center"
+                        borderRadius="3px"
+                    >
+                        23
+                    </Box>
+                </Flex>
                 <SimpleGrid minChildWidth="253px" spacing="20px">
                     <VideoCard />
                     <VideoCard />
