713d6171655a99bc4c2ae93b1a5ce0de4b2f4f68
XCODE89 <maph0989@gmail.com>
2024-08-22 13:31:50 -0400

    OV-9: * get the user from store

--- frontend/src/router/components/protected-route.tsx
+++ frontend/src/router/components/protected-route.tsx
@@ -2,13 +2,15 @@ import { Navigate } from 'react-router-dom';
 
 import { RouterOutlet } from '~/bundles/common/components/components.js';
 import { AppRoute } from '~/bundles/common/enums/app-route.enum.js';
+// import { useAppSelector } from '~/bundles/common/hooks/hooks.js';
 
 const ProtectedRoute: React.FC = () => {
-    // TODO: When the JWT is implemented and the token is stored in local storage, it should be activated.
-    // const isAuth = !!localStorage.getItem('token');
-    const isAuth = true;
+    // const user = useAppSelector((state) => state.auth.user);
 
-    if (!isAuth) {
+    // TODO: for implementing persistence. The following line is temporary
+    const user = true;
+
+    if (!user) {
         return <Navigate to={AppRoute.SIGN_IN} replace />;
     }
 
