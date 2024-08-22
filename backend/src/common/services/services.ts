316a28eb4791feec3905084edeac8ced675171b0
Sergiy <sergiy0378@gmail.com>
2024-08-22 23:40:36 +0300

    OV-39: + move files

--- backend/src/common/services/services.ts
+++ backend/src/common/services/services.ts
@@ -1,10 +1,5 @@
-import { config } from '~/common/config/config.js';
-
 import { CryptService } from './crypt/crypt.service.js';
-import { OpenAIService } from './open-ai/open-ai.service.js';
 
-const openAIService = new OpenAIService({ config });
 const cryptService = new CryptService();
 
 export { cryptService };
-export { openAIService };
