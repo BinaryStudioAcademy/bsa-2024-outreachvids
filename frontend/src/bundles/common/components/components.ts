70576307b943d69b4e1331a997796275d815b6ce
JKaypa <jose_kaypa@hotmail.com>
2024-08-21 22:12:09 -0500

    OV-13: + export components

--- frontend/src/bundles/common/components/components.ts
+++ frontend/src/bundles/common/components/components.ts
@@ -1,13 +1,16 @@
 export { Button } from './button/button.js';
 export { Header } from './header/header.js';
+export { IconMap } from './icon/icon.js';
 export { Input } from './input/input.js';
 export { Link } from './link/link.js';
 export { Loader } from './loader/loader.js';
 export { Overlay } from './overlay/overlay.js';
 export { RouterProvider } from './router-provider/router-provider.js';
+export { SideBar } from './side-bar/side-bar.js';
 export { VideoModal } from './video-modal/video-modal.js';
 export { DownloadIcon } from '@chakra-ui/icons';
 export { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+export { Icon } from '@chakra-ui/react';
 export {
     Box,
     Center,
@@ -21,6 +24,7 @@ export {
     InputGroup,
     InputRightElement,
     SimpleGrid,
+    Spacer,
     Text,
     VStack,
 } from '@chakra-ui/react';
