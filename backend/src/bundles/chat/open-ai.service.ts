e69ce2a463df7a35bf4cc9f17eab72c9abb8980a
Sergiy <sergiy0378@gmail.com>
2024-08-23 17:14:58 +0300

    OV-52: + open ai service

--- backend/src/bundles/chat/open-ai.service.ts
+++ backend/src/bundles/chat/open-ai.service.ts
@@ -1,23 +1,20 @@
 import OpenAI, { type OpenAI as OpenAIType } from 'openai';
+import { type ValueOf } from 'shared';
 
 import { type BaseConfig } from '~/common/config/base-config.package.js';
 
 import { CHAT_MODEL, MAX_TOKEN } from './libs/constants/constants.js';
+import { type OpenAIRole } from './libs/enums/open-ai-role.enum.js';
 import {
-    type GeneratedText,
     type Message,
     type OpenAIService as OpenAIServiceModule,
 } from './libs/types/types.js';
-
-type Constructor = {
-    config: BaseConfig;
-};
 
 class OpenAIService implements OpenAIServiceModule {
     private config: BaseConfig;
     private openAi: OpenAIType;
 
-    public constructor({ config }: Constructor) {
+    public constructor(config: BaseConfig) {
         this.config = config;
         this.openAi = this.initOpenAi();
     }
@@ -28,15 +25,27 @@ class OpenAIService implements OpenAIServiceModule {
         });
     };
 
-    public async generateText(messages: Message[]): Promise<GeneratedText> {
+    public addMessageToHistory(
+        chatHistory: Message[],
+        userMessage: string,
+        role: ValueOf<typeof OpenAIRole>,
+    ): void {
+        const newUserMessage = {
+            content: userMessage,
+            role,
+        };
+
+        chatHistory.push(newUserMessage);
+    }
+
+    public async generateText(messages: Message[]): Promise<string> {
         const completion = await this.openAi.chat.completions.create({
             messages,
             model: CHAT_MODEL,
             max_tokens: MAX_TOKEN,
         });
 
-        const generatedText = completion.choices[0]?.message.content || '';
-        return { generatedText };
+        return completion.choices[0]?.message.content || '';
     }
 }
 
--- backend/src/bundles/open-ai/libs/types/types.ts
+++
@@ -1,3 +0,0 @@
-export { type GeneratedText } from './generated-text.type.js';
-export { type Message } from './message.type.js';
-export { type OpenAIService } from './open-ai-service.type.js';
