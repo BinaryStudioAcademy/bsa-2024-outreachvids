8c34639fbed963089c63c04be68daffc7d95cd46
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-08-22 22:23:55 -0500

    OV-31: - delete sidebar component

--- frontend/src/bundles/video_editor/components/timeline.tsx
+++ frontend/src/bundles/video_editor/components/timeline.tsx
@@ -5,7 +5,6 @@ import { useMemo } from 'react';
 
 import { Item } from './item.js';
 import { Row } from './row.js';
-import { Sidebar } from './sidebar.js';
 import { Subrow } from './subrow.js';
 import { type MarkerDefinition } from './timeaxis.js';
 import { TimeAxis } from './timeaxis.js';
@@ -60,6 +59,7 @@ interface TimelineProperties {
 	rows: RowDefinition[];
 	items: ItemDefinition[];
 }
+
 const Timeline = (properties: TimelineProperties): JSX.Element => {
     const { setTimelineRef, style, range } = useTimelineContext();
 
@@ -67,13 +67,13 @@ const Timeline = (properties: TimelineProperties): JSX.Element => {
         () => groupItemsToSubrows(properties.items, range),
         [properties.items, range],
     );
-
     return (
         <div ref={setTimelineRef} style={style}>
             <TimeAxis markers={timeAxisMarkers} />
             <TimeCursor />
+
             {properties.rows.map((row) => (
-                <Row id={row.id} key={row.id} sidebar={<Sidebar row={row} />}>
+                <Row id={row.id} key={row.id} >
                     {groupedSubrows[row.id]?.map((subrow, index) => (
                         <Subrow key={`${row.id}-${index}`}>
                             {subrow.map((item) => (
