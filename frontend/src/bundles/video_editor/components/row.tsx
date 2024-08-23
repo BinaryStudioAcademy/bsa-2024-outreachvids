00da8cb55fbc99f8856d72b9193d0b9eaf53e673
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-08-22 22:23:02 -0500

    OV-31: + add custom style to rows

--- frontend/src/bundles/video_editor/components/row.tsx
+++ frontend/src/bundles/video_editor/components/row.tsx
@@ -3,23 +3,17 @@ import { useRow } from 'dnd-timeline';
 
 interface RowProperties extends RowDefinition {
 	children: React.ReactNode;
-	sidebar: React.ReactNode;
 }
 const Row = (properties: RowProperties): JSX.Element => {
     const {
         setNodeRef,
-        setSidebarRef,
         rowWrapperStyle,
         rowStyle,
-        rowSidebarStyle,
     } = useRow({ id: properties.id });
 
     return (
         <div style={{ ...rowWrapperStyle, minHeight: 50 }}>
-            <div ref={setSidebarRef} style={rowSidebarStyle}>
-                {properties.sidebar}
-            </div>
-            <div ref={setNodeRef} style={{ ...rowStyle, border: '1px solid grey' }}>
+            <div ref={setNodeRef} style={{ ...rowStyle, border: '1px solid white' }}>
                 {properties.children}
             </div>
         </div>
