37c3dd9a5ab9f720de2127990f3f10bae3af04df
Sergiy <sergiy0378@gmail.com>
2024-08-24 14:19:05 +0300

    OV-52: - sessionChatHistory type

--- backend/src/bundles/chat/libs/types/types.ts
+++ backend/src/bundles/chat/libs/types/types.ts
@@ -1,4 +1,3 @@
 export { type Message } from './message.type.js';
 export { type OpenAIService } from './open-ai-service.type.js';
-export { type SessionChatHistory } from './session-chat-history.js';
 export { type GenerateTextRequestDto } from 'shared';
