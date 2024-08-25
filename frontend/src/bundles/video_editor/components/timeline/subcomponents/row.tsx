5c95eac42d095dd7d435f65b6bebdba713f6c778
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-08-25 18:37:46 -0500

    OV-31: + create row component

---
+++ frontend/src/bundles/video_editor/components/timeline/subcomponents/row.tsx
@@ -0,0 +1,23 @@
+import { type RowDefinition } from 'dnd-timeline';
+import { useRow } from 'dnd-timeline';
+
+interface RowProperties extends RowDefinition {
+	children: React.ReactNode;
+}
+const Row = (properties: RowProperties): JSX.Element => {
+    const {
+        setNodeRef,
+        rowWrapperStyle,
+        rowStyle,
+    } = useRow({ id: properties.id });
+
+    return (
+        <div style={{ ...rowWrapperStyle, minHeight: 50 }}>
+            <div ref={setNodeRef} style={{ ...rowStyle, border: '1px solid white' }}>
+                {properties.children}
+            </div>
+        </div>
+    );
+};
+
+export { Row };
