3ef2954dac7fc3d41e93709a9331f464f0b97aa5
Sergiy <sergiy0378@gmail.com>
2024-08-26 21:32:48 +0300

    OV-52: * openAI service

--- backend/src/bundles/chat/libs/types/open-ai-service.type.ts
+++ backend/src/bundles/chat/libs/types/open-ai-service.type.ts
@@ -1,17 +1,7 @@
-import { type ValueOf } from 'shared';
-
-import { type OpenAIRole } from '../enums/enums.js';
 import { type Message } from './message.type.js';
 
 type OpenAIService = {
     generateText(messages: Message[]): Promise<string>;
-    addMessageToHistory(
-        chatHistory: Message[],
-        userMessage: string,
-        role: ValueOf<typeof OpenAIRole>,
-    ): void;
-    clearChatHistory(messages: Message[]): void;
-    deleteOldMessages(messages: Message[], maxTokens: number): void;
 };
 
 export { type OpenAIService };
--- backend/src/bundles/chat/open-ai.service.ts
+++ backend/src/bundles/chat/open-ai.service.ts
@@ -1,11 +1,8 @@
 import OpenAI, { type OpenAI as OpenAIType } from 'openai';
-import { type ValueOf } from 'shared';
-import { type Tiktoken, encoding_for_model } from 'tiktoken';
 
 import { type BaseConfig } from '~/common/config/base-config.package.js';
 
 import { CHAT_MODEL, MAX_TOKEN } from './libs/constants/constants.js';
-import { type OpenAIRole } from './libs/enums/open-ai-role.enum.js';
 import {
     type Message,
     type OpenAIService as OpenAIServiceModule,
@@ -14,12 +11,10 @@ import {
 class OpenAIService implements OpenAIServiceModule {
     private config: BaseConfig;
     private openAi: OpenAIType;
-    private modelEncoding: Tiktoken;
 
     public constructor(config: BaseConfig) {
         this.config = config;
         this.openAi = this.initOpenAi();
-        this.modelEncoding = encoding_for_model('gpt-4o-mini');
     }
 
     private initOpenAi = (): OpenAIType => {
@@ -28,23 +23,6 @@ class OpenAIService implements OpenAIServiceModule {
         });
     };
 
-    public addMessageToHistory(
-        chatHistory: Message[],
-        userMessage: string,
-        role: ValueOf<typeof OpenAIRole>,
-    ): void {
-        const newUserMessage = {
-            content: userMessage,
-            role,
-        };
-
-        chatHistory.push(newUserMessage);
-    }
-
-    public clearChatHistory(messages: Message[]): void {
-        messages.splice(0, messages.length);
-    }
-
     public async generateText(messages: Message[]): Promise<string> {
         const completion = await this.openAi.chat.completions.create({
             messages,
@@ -52,32 +30,6 @@ class OpenAIService implements OpenAIServiceModule {
             max_tokens: MAX_TOKEN,
         });
         return completion.choices[0]?.message.content || '';
-    }
-
-    private countTokens(messages: Message[]): number {
-        return messages.reduce(
-            (sum, message) =>
-                sum + this.modelEncoding.encode(message.content).length,
-            0,
-        );
-    }
-
-    public deleteOldMessages(messages: Message[], maxTokens: number): void {
-        let totalTokens = this.countTokens(messages);
-
-        while (totalTokens > maxTokens) {
-            const removedMessage = messages[0];
-
-            if (!removedMessage) {
-                break;
-            }
-
-            messages.splice(0, 1);
-
-            totalTokens -= this.modelEncoding.encode(
-                removedMessage.content,
-            ).length;
-        }
     }
 }
 
