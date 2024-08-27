c0ec426f9552d63ceed33dfa34fe8d6e726de655
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-08-27 18:27:38 +0200

    OV-59: * fix typo to required

--- shared/src/bundles/videos/enums/video-validation-message.enum.ts
+++ shared/src/bundles/videos/enums/video-validation-message.enum.ts
@@ -1,10 +1,10 @@
 const VideoValidationMessage = {
     VIDEO_DOESNT_EXIST: 'Video with this id does not exist',
-    USER_ID_REQUIRE: 'User id is required',
+    USER_ID_REQUIRED: 'User id is required',
     UUID_WRONG: 'Invalid UUID',
-    NAME_REQUIRE: 'The video name is required',
+    NAME_REQUIRED: 'The video name is required',
     NAME_LENGTH: 'The video name should have at least 1 character',
-    URL_REQUIRE: 'The video url is required',
+    URL_REQUIRED: 'The video url is required',
     URL_WRONG: 'Invalid url',
 } as const;
 
--- shared/src/bundles/videos/validation-schemas/create-video.validation-schema.ts
+++ shared/src/bundles/videos/validation-schemas/create-video.validation-schema.ts
@@ -11,17 +11,17 @@ type CreateVideoValidationDto = {
 const createVideo = z
     .object<CreateVideoValidationDto>({
         userId: z
-            .string({ required_error: VideoValidationMessage.USER_ID_REQUIRE })
+            .string({ required_error: VideoValidationMessage.USER_ID_REQUIRED })
             .trim()
             .uuid({ message: VideoValidationMessage.UUID_WRONG }),
         name: z
-            .string({ required_error: VideoValidationMessage.NAME_REQUIRE })
+            .string({ required_error: VideoValidationMessage.NAME_REQUIRED })
             .trim()
             .min(1, {
                 message: VideoValidationMessage.NAME_LENGTH,
             }),
         url: z
-            .string({ required_error: VideoValidationMessage.URL_REQUIRE })
+            .string({ required_error: VideoValidationMessage.URL_REQUIRED })
             .trim()
             .url({ message: VideoValidationMessage.URL_WRONG }),
     })
