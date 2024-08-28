0631d153ef143147ac6ddd4ff05fdb7c2a23f639
XCODE89 <maph0989@gmail.com>
2024-08-28 03:24:15 -0400

    OV-44: * Routes in an object

--- frontend/src/router/components/protected-route.tsx
+++ frontend/src/router/components/protected-route.tsx
@@ -1,18 +1,14 @@
-import {
-    Navigate,
-    RouterOutlet,
-} from '~/bundles/common/components/components.js';
+import { Navigate } from '~/bundles/common/components/components.js';
 import { AppRoute } from '~/bundles/common/enums/enums.js';
 
-const ProtectedRoute: React.FC = () => {
+interface Properties {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<Properties> = ({ children }) => {
     // TODO: When persistence is implemented, the user will be taken from the store. The following line is temporary
     const user = true;
-
-    if (!user) {
-        return <Navigate to={AppRoute.SIGN_IN} replace />;
-    }
-
-    return <RouterOutlet />;
+    return user ? children : <Navigate to={AppRoute.SIGN_IN} replace />;
 };
 
 export { ProtectedRoute };
