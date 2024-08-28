4c2fded22947e388c9223bdcdd9adef066d5dee5
JKaypa <jose_kaypa@hotmail.com>
2024-08-28 16:02:54 -0500

    OV-62: + integrate sidebar to my avatar page

--- frontend/src/bundles/my-avatar/pages/my-avatar.tsx
+++ frontend/src/bundles/my-avatar/pages/my-avatar.tsx
@@ -1,17 +1,30 @@
-import { Box, Header, Text } from '~/bundles/common/components/components.js';
+import {
+    Box,
+    Header,
+    Sidebar,
+    Text,
+} from '~/bundles/common/components/components.js';
 
 import { CreateAvatar } from '../components/components.js';
 
 const MyAvatar = (): JSX.Element => {
     return (
-        <Box bg="background.900" p="0 25px">
+        <>
             <Header />
-            {/* Sidebar */}
-            <Box bg="background.50" height="100vh" p="25px" borderRadius="xl">
-                <Text variant="title">My Avatar</Text>
-                <CreateAvatar />
-            </Box>
-        </Box>
+            <Sidebar>
+                <Box bg="background.900" pr="20px">
+                    <Box
+                        bg="background.50"
+                        height="100vh"
+                        p="20px"
+                        borderRadius="xl"
+                    >
+                        <Text variant="title">My Avatar</Text>
+                        <CreateAvatar />
+                    </Box>
+                </Box>
+            </Sidebar>
+        </>
     );
 };
 
