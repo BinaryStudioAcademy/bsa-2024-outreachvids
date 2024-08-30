ee2569654e9d135d3798b6539fdcfd39f6265d52
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-08-30 16:27:01 +0300

    OV-101: + add video-editor to studio page

--- frontend/src/bundles/studio/pages/studio.tsx
+++ frontend/src/bundles/studio/pages/studio.tsx
@@ -6,6 +6,8 @@ import {
 } from '~/bundles/common/components/components.js';
 import { IconName } from '~/bundles/common/icons/icons.js';
 
+import { VideoEditor } from '../components/video-editor/video-editor.js';
+
 const Studio: React.FC = () => {
     return (
         <>
@@ -25,6 +27,7 @@ const Studio: React.FC = () => {
                     />
                 }
             />
+            <VideoEditor />
         </>
     );
 };
