9b111d7b08c920849232da4028251f8c90ef806a
Sergiy <sergiy0378@gmail.com>
2024-08-30 12:51:45 +0300

    OV-74: * icons

--- frontend/src/bundles/common/icons/icon-name.ts
+++ frontend/src/bundles/common/icons/icon-name.ts
@@ -1,6 +1,19 @@
-import { faEllipsisVertical, faPen } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCloudArrowDown,
+    faEllipsisVertical,
+    faPause,
+    faPen,
+    faPlay,
+    faVolumeHigh,
+    faVolumeOff,
+} from '@fortawesome/free-solid-svg-icons';
 
 const IconName = {
+    PLAY: faPlay,
+    PAUSE: faPause,
+    VOLUME: faVolumeHigh,
+    VOLUME_OFF: faVolumeOff,
+    CLOUD_ARROW_DOWN: faCloudArrowDown,
     OPTIONS_VERTICAL: faEllipsisVertical,
     PEN: faPen,
 } as const;
