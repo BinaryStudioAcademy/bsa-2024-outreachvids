bb1bc6e666b389749ef69e0ef6391422f273bfef
Albina <alblupynos@gmail.com>
2024-08-31 15:25:17 +0300

    OV-51: + add example dispatch to get current user

--- frontend/src/bundles/home/pages/home.tsx
+++ frontend/src/bundles/home/pages/home.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+
+import { actions as authActions } from '~/bundles/auth/store/auth.js';
 import { Box, Header } from '~/bundles/common/components/components.js';
+import { useAppDispatch } from '~/bundles/common/hooks/hooks.js';
 
 import { MainContent } from '../components/components.js';
 
 const Home: React.FC = () => {
+    // TODO: Move dispatch to place where current user is displayed
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        void dispatch(authActions.loadCurrentUser());
+    });
     return (
         <Box bg="background.900" height="100vh">
             <Header />
