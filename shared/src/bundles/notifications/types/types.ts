728a5b44eaec6ecedb01ebd3a8de54c7cbfd5d16
JKaypa <jose_kaypa@hotmail.com>
2024-09-01 18:23:29 -0500

    OV-97: + create notifiactions types

---
+++ shared/src/bundles/notifications/types/types.ts
@@ -0,0 +1,6 @@
+export { type CreateNotificationRequestDto } from './create-notification-request-dto.type.js';
+export { type NotificationGetAllItemResponseDto } from './notification-get-all-item-response-dto.type.js';
+export { type NotificationGetAllResponseDto } from './notification-get-all-response-dto.type.js';
+export { type NotificationGetOneRequestDto } from './notification-get-one-request-dto.type.js';
+export { type NotificationType } from './notification-type.type.js';
+export { type UpdateNotificationRequestDto } from './update-notification-request-dto.type.js';
