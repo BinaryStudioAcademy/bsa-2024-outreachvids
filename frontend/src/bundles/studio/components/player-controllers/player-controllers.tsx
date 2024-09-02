1a20072f5265fb459a30083205afd9780fbe3d48
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-02 17:39:03 +0200

    OV-125: * add time

--- frontend/src/bundles/studio/components/player-controllers/player-controllers.tsx
+++ frontend/src/bundles/studio/components/player-controllers/player-controllers.tsx
@@ -4,6 +4,7 @@ import {
     Flex,
     Icon,
     IconButton,
+    Text,
 } from '~/bundles/common/components/components.js';
 import { IconName } from '~/bundles/common/icons/icons.js';
 
@@ -12,36 +13,55 @@ const PlayerControllers: React.FC = () => {
         <Flex
             justifyContent="center"
             alignItems="center"
-            gap="11px"
+            gap="15px"
             height="45px"
-            borderColor="color.background[50]"
+            borderColor="background.50"
             borderWidth="1px"
         >
-            <IconButton
-                aria-label="Step back"
-                isRound={true}
-                size="xs"
-                variant="gray"
-                icon={
-                    <Icon as={FontAwesomeIcon} icon={IconName.PLAY_STEP_BACK} />
-                }
-            />
-            <IconButton
-                aria-label="Play"
-                isRound={true}
-                size="sm"
-                variant="gray"
-                icon={<Icon as={FontAwesomeIcon} icon={IconName.PLAY} />}
-            />
-            <IconButton
-                aria-label="Step next"
-                isRound={true}
-                size="xs"
-                variant="gray"
-                icon={
-                    <Icon as={FontAwesomeIcon} icon={IconName.PLAY_STEP_NEXT} />
-                }
-            />
+            <Flex alignItems="center" gap="11px">
+                <IconButton
+                    aria-label="Step back"
+                    isRound={true}
+                    size="xs"
+                    variant="gray"
+                    icon={
+                        <Icon
+                            as={FontAwesomeIcon}
+                            icon={IconName.PLAY_STEP_BACK}
+                        />
+                    }
+                />
+                <IconButton
+                    aria-label="Play"
+                    isRound={true}
+                    size="sm"
+                    variant="gray"
+                    icon={<Icon as={FontAwesomeIcon} icon={IconName.PLAY} />}
+                />
+                <IconButton
+                    aria-label="Step next"
+                    isRound={true}
+                    size="xs"
+                    variant="gray"
+                    icon={
+                        <Icon
+                            as={FontAwesomeIcon}
+                            icon={IconName.PLAY_STEP_NEXT}
+                        />
+                    }
+                />
+            </Flex>
+            <Flex gap="2px">
+                <Text color="typography.900" variant="caption">
+                    00:00
+                </Text>
+                <Text color="background.50" variant="caption">
+                    /
+                </Text>
+                <Text color="background.50" variant="caption">
+                    00:10
+                </Text>
+            </Flex>
         </Flex>
     );
 };
