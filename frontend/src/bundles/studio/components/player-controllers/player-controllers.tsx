53b203240f424ebec9547eabaf8a372ae7ce825b
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-02 16:25:24 +0200

    OV-125: + player controllers component

---
+++ frontend/src/bundles/studio/components/player-controllers/player-controllers.tsx
@@ -0,0 +1,49 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+    Flex,
+    Icon,
+    IconButton,
+} from '~/bundles/common/components/components.js';
+import { IconName } from '~/bundles/common/icons/icons.js';
+
+const PlayerControllers: React.FC = () => {
+    return (
+        <Flex
+            justifyContent="center"
+            alignItems="center"
+            gap="11px"
+            height="45px"
+            borderColor="color.background[50]"
+            borderWidth="1px"
+        >
+            <IconButton
+                aria-label="Step back"
+                isRound={true}
+                size="xs"
+                variant="gray"
+                icon={
+                    <Icon as={FontAwesomeIcon} icon={IconName.PLAY_STEP_BACK} />
+                }
+            />
+            <IconButton
+                aria-label="Play"
+                isRound={true}
+                size="sm"
+                variant="gray"
+                icon={<Icon as={FontAwesomeIcon} icon={IconName.PLAY} />}
+            />
+            <IconButton
+                aria-label="Step next"
+                isRound={true}
+                size="xs"
+                variant="gray"
+                icon={
+                    <Icon as={FontAwesomeIcon} icon={IconName.PLAY_STEP_NEXT} />
+                }
+            />
+        </Flex>
+    );
+};
+
+export { PlayerControllers };
