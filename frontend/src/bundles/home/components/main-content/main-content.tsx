de22bad98a267c91ae8098c5bb7feb1117dc9ba8
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-02 13:08:41 -0500

    OV-75: - remove left margin from main content

--- frontend/src/bundles/home/components/main-content/main-content.tsx
+++ frontend/src/bundles/home/components/main-content/main-content.tsx
@@ -4,7 +4,7 @@ import { VideoSection } from '../components.js';
 
 const MainContent: React.FC = () => {
     return (
-        <Box bg="background.50" borderRadius="lg" margin="0 25px">
+        <Box bg="background.50" borderRadius="lg">
             <VideoSection />
             <VideoSection />
         </Box>
