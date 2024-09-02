7b7c4279a9431b109993f1d25543d8be06cf20ab
Wilson Bravo <bravoc.wilson@gmail.com>
2024-09-01 23:06:09 -0500

    OV-105: + avatar types enums

---
+++ backend/src/bundles/avatars/types/types.ts
@@ -0,0 +1,7 @@
+export { type Avatar } from './avatar.type.js';
+export { type Style } from './style.type.js';
+export {
+    type AvatarGetAllResponseDto,
+    type AvatarGetOneRequestDto,
+    type AvatarGetResponseDto,
+} from 'shared';
