5775ca1fcf63e5ccfd7334311e5f57814d890310
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-02 19:14:33 +0200

    OV-125: + format time helper

--- frontend/src/bundles/studio/components/player-controllers/components/time-display.tsx
+++ frontend/src/bundles/studio/components/player-controllers/components/time-display.tsx
@@ -1,16 +1,18 @@
 import { Flex, Text } from '~/bundles/common/components/components.js';
 
+import { formatTime } from '../helpers/format-time.js';
+
 const TimeDisplay: React.FC = () => {
     return (
         <Flex gap="2px" position="absolute" left="100%" marginLeft="15px">
             <Text color="typography.900" variant="caption">
-                00:00
+                {formatTime(5)}
             </Text>
             <Text color="background.50" variant="caption">
                 /
             </Text>
             <Text color="background.50" variant="caption">
-                00:10
+                {formatTime(10)}
             </Text>
         </Flex>
     );
---
+++ frontend/src/bundles/studio/components/player-controllers/helpers/format-time.ts
@@ -0,0 +1,7 @@
+const formatTime = (totalSeconds: number): string => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+};
+
+export { formatTime };
