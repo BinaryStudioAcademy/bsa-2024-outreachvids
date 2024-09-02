20d30be0a3d8888b1835457eed96f18735ef9e2e
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-02 18:33:43 +0200

    OV-125: * switch to pause and play on click

--- frontend/src/bundles/studio/components/player-controllers/player-controllers.tsx
+++ frontend/src/bundles/studio/components/player-controllers/player-controllers.tsx
@@ -5,11 +5,18 @@ import {
     Icon,
     IconButton,
 } from '~/bundles/common/components/components.js';
+import { useCallback, useState } from '~/bundles/common/hooks/hooks.js';
 import { IconName } from '~/bundles/common/icons/icons.js';
 
 import { TimeDisplay } from './components/components.js';
 
 const PlayerControllers: React.FC = () => {
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+    const handleClick = useCallback((): void => {
+        setIsPlaying((previous) => !previous);
+    }, []);
+
     return (
         <Flex
             justifyContent="center"
@@ -32,11 +39,17 @@ const PlayerControllers: React.FC = () => {
                     }
                 />
                 <IconButton
-                    aria-label="Play"
+                    aria-label={isPlaying ? 'Pause' : 'Play'}
                     isRound={true}
                     size="sm"
                     variant="gray"
-                    icon={<Icon as={FontAwesomeIcon} icon={IconName.PLAY} />}
+                    icon={
+                        <Icon
+                            as={FontAwesomeIcon}
+                            icon={isPlaying ? IconName.PAUSE : IconName.PLAY}
+                        />
+                    }
+                    onClick={handleClick}
                 />
                 <IconButton
                     aria-label="Step next"
