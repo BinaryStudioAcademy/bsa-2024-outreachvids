308c12b0cafad10626f0914e2c59e9532aa66167
JKaypa <jose_kaypa@hotmail.com>
2024-09-01 21:04:52 -0500

    OV-97: + create notification controller

---
+++ backend/src/bundles/notifications/notification.controller.ts
@@ -0,0 +1,220 @@
+import { type NotificationService } from '~/bundles/notifications/notification.service.js';
+import {
+    type ApiHandlerOptions,
+    type ApiHandlerResponse,
+    BaseController,
+} from '~/common/controller/controller.js';
+import { ApiPath } from '~/common/enums/enums.js';
+import { HttpCode } from '~/common/http/http.js';
+import { type Logger } from '~/common/logger/logger.js';
+import { idParametersValidationSchema } from '~/common/validation-schemas/validation-schemas.js';
+
+import { NotificationsApiPath } from './enums/enums.js';
+import {
+    type CreateNotificationRequestDto,
+    type NotificationGetOneRequestDto,
+    type UpdateNotificationRequestDto,
+} from './types/types.js';
+import {
+    createNotificationValidationSchema,
+    updateNotificationValidationSchema,
+} from './validation-schemas/validation-schemas.js';
+
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      Notification:
+ *        type: object
+ *        properties:
+ *          id:
+ *            type: string
+ *            format: uuid
+ *          userId:
+ *            type: string
+ *            format: uuid
+ *          isRead:
+ *            type: boolean
+ *            default: false
+ *          type:
+ *            type: string
+ *            enum: ['render']
+ */
+
+class NotificationController extends BaseController {
+    private notificationService: NotificationService;
+
+    public constructor(
+        logger: Logger,
+        notificationService: NotificationService,
+    ) {
+        super(logger, ApiPath.NOTIFICATIONS);
+
+        this.notificationService = notificationService;
+
+        this.addRoute({
+            path: NotificationsApiPath.ROOT,
+            method: 'GET',
+            handler: () => this.findAll(),
+        });
+
+        this.addRoute({
+            path: NotificationsApiPath.ROOT,
+            method: 'POST',
+            validation: {
+                body: createNotificationValidationSchema,
+            },
+            handler: (options) =>
+                this.create(
+                    options as ApiHandlerOptions<{
+                        body: CreateNotificationRequestDto;
+                    }>,
+                ),
+        });
+
+        this.addRoute({
+            path: NotificationsApiPath.ID,
+            method: 'PATCH',
+            validation: {
+                body: updateNotificationValidationSchema,
+                params: idParametersValidationSchema,
+            },
+            handler: (options) =>
+                this.update(
+                    options as ApiHandlerOptions<{
+                        body: UpdateNotificationRequestDto;
+                        params: NotificationGetOneRequestDto;
+                    }>,
+                ),
+        });
+    }
+
+    /**
+     * @swagger
+     * /notifications:
+     *    get:
+     *      description: Get all unread notifications
+     *      responses:
+     *        200:
+     *          description: Successful operation
+     *          content:
+     *            application/json:
+     *              schema:
+     *                type: object
+     *                properties:
+     *                  items:
+     *                    type: array
+     *                    description: A list of unread notification objects
+     *                    items:
+     *                      $ref: '#/components/schemas/Notification'
+     */
+    private async findAll(): Promise<ApiHandlerResponse> {
+        return {
+            status: HttpCode.OK,
+            payload: await this.notificationService.findAll(),
+        };
+    }
+
+    /**
+     * @swagger
+     * /notifications:
+     *    post:
+     *      description: Create new notification
+     *      requestBody:
+     *        description: Notification data
+     *        required: true
+     *        content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              required: [userId, isRead, type]
+     *              properties:
+     *                userId:
+     *                  type: string
+     *                  format: uuid
+     *                isRead:
+     *                  type: boolean
+     *                  default: false
+     *                type:
+     *                  type: string
+     *                  enum [render]
+     *      responses:
+     *        201:
+     *          description: Successful operation
+     *          content:
+     *            application/json:
+     *              schema:
+     *                $ref: '#/components/schemas/Notification'
+     */
+    private async create(
+        options: ApiHandlerOptions<{
+            body: CreateNotificationRequestDto;
+        }>,
+    ): Promise<ApiHandlerResponse> {
+        return {
+            status: HttpCode.CREATED,
+            payload: await this.notificationService.create(options.body),
+        };
+    }
+
+    /**
+     * @swagger
+     * /notifications/{id}:
+     *    patch:
+     *      parameters:
+     *        - in: path
+     *          name: id
+     *          required: true
+     *          schema:
+     *            type: string
+     *            format: uuid
+     *          description: The notification id
+     *      description: Update notification by id
+     *      requestBody:
+     *        description: Notification data
+     *        content:
+     *          application/json:
+     *            schema:
+     *              type: object
+     *              properties:
+     *                userId:
+     *                  type: string
+     *                  format: uuid
+     *                isRead:
+     *                  type: boolean
+     *                  default: false
+     *                type:
+     *                  type: string
+     *                  enum: [render]
+     *      responses:
+     *        200:
+     *          description: Successful operation
+     *          content:
+     *            application/json:
+     *              schema:
+     *                $ref: '#/components/schemas/Notification'
+     *        404:
+     *          description: Failed operation. The resource was not found.
+     *          content:
+     *            application/json:
+     *              schema:
+     *                  type: object
+     *                  $ref: '#/components/schemas/Error'
+     */
+    private async update(
+        options: ApiHandlerOptions<{
+            params: NotificationGetOneRequestDto;
+            body: UpdateNotificationRequestDto;
+        }>,
+    ): Promise<ApiHandlerResponse> {
+        return {
+            status: HttpCode.OK,
+            payload: await this.notificationService.update(
+                options.params.id,
+                options.body,
+            ),
+        };
+    }
+}
+
+export { NotificationController };
