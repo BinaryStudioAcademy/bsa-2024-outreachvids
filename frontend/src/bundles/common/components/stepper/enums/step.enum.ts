0c4985639b16c92daee75532610aaa308c0042a2
Wilson Bravo <bravoc.wilson@gmail.com>
2024-09-02 23:08:08 -0500

    OV-61: - custom stepper

--- frontend/src/bundles/common/components/stepper/enums/step.enum.ts
+++
@@ -1,9 +0,0 @@
-const Step = {
-    COMPLETED: 'completed',
-    CURRENT: 'current',
-    DEFAULT: 'default',
-    LAST_UNCOMPLETED: 'lastUncompleted',
-    LAST_COMPLETED: 'lastCompleted',
-} as const;
-
-export { Step };
