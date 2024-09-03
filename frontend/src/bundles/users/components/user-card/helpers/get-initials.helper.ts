6c86f67c68daeb4c406e56bfc34510f449a42181
JKaypa <jose_kaypa@hotmail.com>
2024-09-03 10:43:06 -0500

    OV-133: * early return

--- frontend/src/bundles/users/components/user-card/helpers/get-initials.helper.ts
+++ frontend/src/bundles/users/components/user-card/helpers/get-initials.helper.ts
@@ -1,12 +1,12 @@
 const getInitials = (fullName: string | undefined): string => {
-    if (fullName) {
-        const [firstName, lastName] = fullName.trim().split(/\s+/);
-        const firstInitial = firstName?.charAt(0).toUpperCase();
-        const secondInitial = lastName?.charAt(0).toUpperCase() ?? '';
-
-        return `${firstInitial}${secondInitial}`;
+    if (!fullName) {
+        return 'FN';
     }
-    return 'FN';
+    const [firstName, lastName] = fullName.trim().split(/\s+/);
+    const firstInitial = firstName?.charAt(0).toUpperCase();
+    const secondInitial = lastName?.charAt(0).toUpperCase() ?? '';
+
+    return `${firstInitial}${secondInitial}`;
 };
 
 export { getInitials };
