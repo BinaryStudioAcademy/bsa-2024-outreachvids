9ac94dbfabf6930bf99116e8a21179813df051af
Wilson Bravo <bravoc.wilson@gmail.com>
2024-09-02 23:09:26 -0500

    OV-61: - get step icon by step name

--- frontend/src/bundles/common/components/stepper/components/components.ts
+++ frontend/src/bundles/common/components/stepper/components/components.ts
@@ -1,2 +1,1 @@
-export { Step } from './step/step.js';
 export { StepIcon } from './step-icon/step-icon.js';
--- frontend/src/bundles/common/components/stepper/components/step-icon/step-icon.tsx
+++ frontend/src/bundles/common/components/stepper/components/step-icon/step-icon.tsx
@@ -1,56 +1,43 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { type ValueOf } from 'shared';
 
 import { Box, Icon } from '~/bundles/common/components/components.js';
 import { IconName } from '~/bundles/common/icons/icons.js';
 
-import { Step } from '../../enums/enums.js';
-
 type Properties = {
-    step: ValueOf<typeof Step>;
+    step: 'complete' | 'active' | 'incomplete';
 };
 
 const StepIcon: React.FC<Properties> = ({ step }) => {
-    if (step === Step.COMPLETED) {
+    if (step === 'complete') {
         return (
-            <Box>
-                <Box backgroundColor="background.900" display="inline-flex">
-                    <Icon as={IconName.CHECK_CIRCLE} boxSize={3} />
-                </Box>
+            <Box backgroundColor="background.900" display="inline-flex">
+                <Icon as={IconName.CHECK_CIRCLE} boxSize={3} />
             </Box>
         );
     }
 
-    if (step === Step.CURRENT || step === Step.LAST_COMPLETED) {
+    if (step === 'active') {
         return (
-            <Box>
-                <Box
-                    sx={{
-                        display: 'inline-flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        padding: '4px',
-                        color: 'white',
-                        border: '2px solid',
-                        borderColor: 'background.600',
-                        borderRadius: '50%',
-                        backgroundColor: 'background.900',
-                    }}
-                >
-                    <Icon
-                        as={FontAwesomeIcon}
-                        icon={IconName.CIRCLE}
-                        boxSize={3}
-                    />
-                </Box>
+            <Box
+                sx={{
+                    display: 'inline-flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '4px',
+                    color: 'white',
+                    border: '2px solid',
+                    borderColor: 'background.600',
+                    borderRadius: '50%',
+                    backgroundColor: 'background.900',
+                }}
+            >
+                <Icon as={FontAwesomeIcon} icon={IconName.CIRCLE} boxSize={3} />
             </Box>
         );
     }
     return (
-        <Box>
-            <Box color="background.600" display="inline-flex">
-                <Icon as={FontAwesomeIcon} icon={IconName.CIRCLE} boxSize={3} />
-            </Box>
+        <Box color="background.600" display="inline-flex">
+            <Icon as={FontAwesomeIcon} icon={IconName.CIRCLE} boxSize={3} />
         </Box>
     );
 };
