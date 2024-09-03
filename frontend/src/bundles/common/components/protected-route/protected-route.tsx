4440ff69ea4de70274f30f930ef71a908b469d19
XCODE89 <maph0989@gmail.com>
2024-09-02 22:11:54 -0400

    OV-114: * loader in protected Route

--- frontend/src/bundles/common/components/protected-route/protected-route.tsx
+++ frontend/src/bundles/common/components/protected-route/protected-route.tsx
@@ -1,5 +1,8 @@
-import { Navigate } from '~/bundles/common/components/components.js';
-import { AppRoute } from '~/bundles/common/enums/enums.js';
+import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { Loader } from '~/bundles/common/components/components.js';
+import { AppRoute, DataStatus } from '~/bundles/common/enums/enums.js';
 
 import { useAppSelector } from '../../hooks/hooks.js';
 
@@ -8,8 +11,23 @@ interface Properties {
 }
 
 const ProtectedRoute: React.FC<Properties> = ({ children }) => {
+    const [loading, setLoading] = useState(true);
     const user = useAppSelector((state) => state.auth.user);
-    return user ? children : <Navigate to={AppRoute.SIGN_IN} replace />;
+    const dataStatus = useAppSelector((state) => state.auth.dataStatus);
+
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    if (loading || dataStatus === DataStatus.PENDING) {
+        return <Loader />;
+    }
+
+    if (!user || dataStatus === DataStatus.REJECTED) {
+        return <Navigate to={AppRoute.SIGN_IN} replace />;
+    }
+
+    return <>{children}</>;
 };
 
 export { ProtectedRoute };
