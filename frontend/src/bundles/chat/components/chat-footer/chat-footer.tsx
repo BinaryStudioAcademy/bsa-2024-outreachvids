4d7144d6826d025a0b43f6cc61fce755715ca0e9
Luis Felix <lfelix3011@gmail.com>
2024-09-02 21:11:23 -0400

    OV-42: - remove chat form and just leave chat footer

--- frontend/src/bundles/chat/components/chat-footer/chat-footer.tsx
+++ frontend/src/bundles/chat/components/chat-footer/chat-footer.tsx
@@ -1,17 +1,59 @@
-import { Box } from '~/bundles/common/components/components.js';
+import {
+    Button,
+    Flex,
+    FormProvider,
+    Input,
+} from '~/bundles/common/components/components.js';
+import { useAppForm, useMemo } from '~/bundles/common/hooks/hooks.js';
 
-import { type GenerateTextRequestDto } from '../../types/types.js';
-import { ChatForm } from '../chat-form/chat-form.js';
+import {
+    type GenerateTextRequestDto,
+    textGenerationValidationSchema,
+} from '../../chat.js';
+import { DEFAULT_CHAT_FORM_PAYLOAD } from './constants/constants.js';
 
 type Properties = {
     onSendMessage: (payload: GenerateTextRequestDto) => void;
 };
 
 const ChatFooter: React.FC<Properties> = ({ onSendMessage }) => {
+    const form = useAppForm<GenerateTextRequestDto>({
+        initialValues: DEFAULT_CHAT_FORM_PAYLOAD,
+        validationSchema: textGenerationValidationSchema,
+        onSubmit: (data: GenerateTextRequestDto, { resetForm }) => {
+            onSendMessage(data);
+            resetForm();
+        },
+    });
+    const { handleSubmit, values } = form;
+
+    const isEmpty = useMemo(
+        () => Object.values(values).some((value) => value.trim().length === 0),
+        [values],
+    );
+
     return (
-        <Box>
-            <ChatForm onSubmit={onSendMessage} />
-        </Box>
+        <FormProvider value={form}>
+            <form onSubmit={handleSubmit}>
+                <Flex alignItems={'flex-start'} w={'100%'} gap={5}>
+                    <Input
+                        type="text"
+                        label=""
+                        placeholder="Send a message"
+                        name="message"
+                        value={values.message}
+                        sx={{ w: '100%' }}
+                    />
+                    <Button
+                        type="submit"
+                        label="Send"
+                        size="md"
+                        isDisabled={isEmpty}
+                        sx={{ w: '100px' }}
+                    />
+                </Flex>
+            </form>
+        </FormProvider>
     );
 };
 
