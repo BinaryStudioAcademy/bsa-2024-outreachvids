3d08158fa2a3023ae9d77d29dee1e86648e5b0c0
Sergiy <sergiy0378@gmail.com>
2024-09-03 21:53:33 +0300

    OV-74: + get-time

--- frontend/src/bundles/common/components/upload-video/components/video-player/components/control/libs/helpers/get-time.ts
+++ frontend/src/bundles/common/components/upload-video/components/video-player/components/control/libs/helpers/get-time.ts
@@ -1,12 +1,16 @@
 import { formatTime } from '~/bundles/common/helpers/format-time.js';
 
+import { MAX_PERCENT } from '../constants/max-percent.js';
+
 type Properties = {
     played: number;
     durationInSeconds: number;
 };
 
 const getTime = ({ played, durationInSeconds }: Properties): string => {
-    const currentSecond = Math.round((played * durationInSeconds) / 100);
+    const currentSecond = Math.round(
+        (played * durationInSeconds) / MAX_PERCENT,
+    );
 
     const currentTime = formatTime(currentSecond);
     const duration = formatTime(durationInSeconds);
