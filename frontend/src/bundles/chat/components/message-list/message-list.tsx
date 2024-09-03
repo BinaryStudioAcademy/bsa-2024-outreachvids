6df9209492b88eb241d10ffc94ac8558ad179788
Luis Felix <lfelix3011@gmail.com>
2024-09-03 12:21:07 -0400

    OV-42: * use of relative path for imports and useMemo to group Messages

--- frontend/src/bundles/chat/components/message-list/message-list.tsx
+++ frontend/src/bundles/chat/components/message-list/message-list.tsx
@@ -1,63 +1,73 @@
+import { MessageBox } from '~/bundles/chat/components/message-box/message-box.js';
+import { MessageSender } from '~/bundles/chat/enums/enums.js';
+import { type Message, type MessageGroup } from '~/bundles/chat/types/types.js';
 import { Flex, VStack } from '~/bundles/common/components/components.js';
+import { useMemo } from '~/bundles/common/hooks/hooks.js';
 import { UserAvatar } from '~/bundles/users/components/components.js';
-
-import { MessageSender } from '../../enums/enums.js';
-import { type Message, type MessageGroup } from '../../types/types.js';
-import { MessageBox } from '../message-box/message-box.js';
 
 type Properties = {
     messages: Message[];
 };
 
 const MessageList: React.FC<Properties> = ({ messages }) => {
-    const groupedMessages: MessageGroup[] = [];
-    let currentGroup: MessageGroup | null = null;
+    const groupedMessages = useMemo(() => {
+        const groupedMessages: MessageGroup[] = [];
+        let currentGroup: MessageGroup | null = null;
 
-    for (const message of messages) {
-        const { sender } = message;
-        if (!currentGroup || currentGroup.sender !== sender) {
-            currentGroup = { sender: sender, messages: [message] };
-            groupedMessages.push(currentGroup);
-        } else {
-            currentGroup.messages.push(message);
+        for (const message of messages) {
+            const { sender } = message;
+            if (!currentGroup || currentGroup.sender !== sender) {
+                currentGroup = { sender, messages: [message] };
+                groupedMessages.push(currentGroup);
+            } else {
+                currentGroup.messages.push(message);
+            }
         }
-    }
+
+        return groupedMessages;
+    }, [messages]);
 
     return (
         <VStack spacing={4} align="stretch" p={4} h="400px" overflowY="auto">
-            {groupedMessages.map((group: MessageGroup, groupIndex: number) => {
-                const { sender, messages } = group;
-                return (
-                    <Flex
-                        key={groupIndex}
-                        direction={
-                            sender === MessageSender.USER
-                                ? 'row-reverse'
-                                : 'row'
-                        }
-                        alignItems="flex-start"
-                    >
-                        <UserAvatar
-                            username={
-                                sender === MessageSender.USER ? 'FL' : 'AI'
-                            }
-                        />
-                        <VStack
-                            spacing={2}
-                            align={
+            {groupedMessages.map(
+                ({ sender, messages }: MessageGroup, groupIndex: number) => {
+                    return (
+                        <Flex
+                            key={groupIndex}
+                            direction={
                                 sender === MessageSender.USER
-                                    ? 'flex-end'
-                                    : 'flex-start'
+                                    ? 'row-reverse'
+                                    : 'row'
                             }
-                            width={'max'}
+                            alignItems="flex-start"
                         >
-                            {messages.map((message: Message, index: number) => (
-                                <MessageBox key={index} message={message} />
-                            ))}
-                        </VStack>
-                    </Flex>
-                );
-            })}
+                            <UserAvatar
+                                username={
+                                    sender === MessageSender.USER ? 'FL' : 'AI'
+                                }
+                            />
+                            <VStack
+                                spacing={2}
+                                align={
+                                    sender === MessageSender.USER
+                                        ? 'flex-end'
+                                        : 'flex-start'
+                                }
+                                width={'max'}
+                            >
+                                {messages.map(
+                                    (message: Message, index: number) => (
+                                        <MessageBox
+                                            key={index}
+                                            message={message}
+                                        />
+                                    ),
+                                )}
+                            </VStack>
+                        </Flex>
+                    );
+                },
+            )}
         </VStack>
     );
 };
