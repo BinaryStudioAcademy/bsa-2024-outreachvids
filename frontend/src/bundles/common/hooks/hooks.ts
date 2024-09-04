dd4dd32cb06f9542ba704a4e76cb8b852722a714
Sergiy <sergiy0378@gmail.com>
2024-09-04 12:20:59 +0300

    OV-122: * hooks

--- frontend/src/bundles/common/hooks/hooks.ts
+++ frontend/src/bundles/common/hooks/hooks.ts
@@ -2,5 +2,5 @@ export { useAppDispatch } from './use-app-dispatch/use-app-dispatch.hook.js';
 export { useAppForm } from './use-app-form/use-app-form.hook.js';
 export { useAppSelector } from './use-app-selector/use-app-selector.hook.js';
 export { useFormField } from './use-form-field/use-form-field.hook.js';
-export { useCallback, useEffect, useMemo, useState } from 'react';
+export { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 export { useLocation, useNavigate } from 'react-router-dom';
