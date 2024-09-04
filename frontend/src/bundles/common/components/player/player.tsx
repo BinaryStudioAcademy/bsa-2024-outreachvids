187f54b015c239d1527f97065ebe7f6799ee228f
Sergiy <sergiy0378@gmail.com>
2024-09-04 12:27:26 +0300

    OV-122: * player

--- frontend/src/bundles/common/components/player/player.tsx
+++ frontend/src/bundles/common/components/player/player.tsx
@@ -1,7 +1,8 @@
 import { type PlayerRef, Player as LibraryPlayer } from '@remotion/player';
 import { type RefObject } from 'react';
 
-import { Box } from '~/bundles/common/components/components.js';
+import { Flex } from '~/bundles/common/components/components.js';
+import { VideoPreview } from '~/bundles/common/enums/enums.js';
 
 type Properties = {
     orientation: 'landscape' | 'portrait';
@@ -17,19 +18,24 @@ const Player = ({
     VideoComponent,
 }: Properties): JSX.Element => {
     const size =
-        orientation === 'landscape'
+        orientation === VideoPreview.LANDSCAPE
             ? { width: 1920, height: 1080 }
             : { width: 1080, height: 1920 };
 
     return (
-        <Box
-            height="720px"
-            width="fit-content"
-            maxWidth="100%"
-            overflow="hidden"
-            border="1px solid #FDFDFD"
+        <Flex
+            alignItems="center"
+            justifyContent="center"
+            width="100%"
+            maxHeight="60vh"
+            height="full"
+            aspectRatio={
+                orientation === VideoPreview.LANDSCAPE ? 16 / 9 : 9 / 16
+            }
+            padding="20px"
         >
             <LibraryPlayer
+                controls
                 ref={playerRef}
                 component={VideoComponent}
                 durationInFrames={durationInFrames}
@@ -37,11 +43,16 @@ const Player = ({
                 compositionHeight={size.height}
                 fps={30}
                 style={{
-                    height: '100%',
-                    maxWidth: '100%',
+                    width:
+                        orientation === VideoPreview.LANDSCAPE ? '40%' : '10%',
+                    minWidth:
+                        orientation === VideoPreview.LANDSCAPE
+                            ? '400px'
+                            : '200px',
+                    objectFit: 'contain',
                 }}
             />
-        </Box>
+        </Flex>
     );
 };
 
