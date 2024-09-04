25aa33dbeca4670bb8af6bd2b291b09c49b97bf4
Sergiy <sergiy0378@gmail.com>
2024-09-04 12:30:46 +0300

    OV-122: + player

--- frontend/src/bundles/studio/pages/studio.tsx
+++ frontend/src/bundles/studio/pages/studio.tsx
@@ -1,16 +1,24 @@
+import { type PlayerRef } from '@remotion/player';
+
 import {
+    Box,
     Button,
     Header,
     Icon,
     IconButton,
+    Player,
 } from '~/bundles/common/components/components.js';
+import { VideoPreview } from '~/bundles/common/enums/enums.js';
+import { useRef } from '~/bundles/common/hooks/hooks.js';
 import { IconName } from '~/bundles/common/icons/icons.js';
 
 import { VideoMenu } from '../components/video-menu/video-menu.js';
+import { VideoComponent } from './components/video.js';
 
 const Studio: React.FC = () => {
+    const playerReference = useRef<PlayerRef>(null);
     return (
-        <>
+        <Box minHeight="100vh" height="100%" position="relative">
             <Header
                 center={
                     <Button
@@ -27,8 +35,16 @@ const Studio: React.FC = () => {
                     />
                 }
             />
+
             <VideoMenu />
-        </>
+
+            <Player
+                VideoComponent={VideoComponent}
+                playerRef={playerReference}
+                durationInFrames={300}
+                orientation={VideoPreview.LANDSCAPE}
+            />
+        </Box>
     );
 };
 
