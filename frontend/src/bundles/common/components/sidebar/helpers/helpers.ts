90bfa287d9a604885521738b0307c99f3af911ca
JKaypa <jose_kaypa@hotmail.com>
2024-09-04 15:02:49 -0500

    OV-140: + create getFlag helper

---
+++ frontend/src/bundles/common/components/sidebar/helpers/helpers.ts
@@ -0,0 +1,16 @@
+import { storage, StorageKey } from '~/framework/storage/storage.js';
+
+import { MD } from '../hooks/use-collapse/constants/constants.js';
+
+const getFlag = async (): Promise<boolean> => {
+    const value = await storage.get(StorageKey.IS_COLLAPSED);
+    if (value) {
+        return JSON.parse(value);
+    }
+
+    const isMobile = window.innerWidth < MD;
+    void storage.set(StorageKey.IS_COLLAPSED, JSON.stringify(isMobile));
+    return isMobile;
+};
+
+export { getFlag };
