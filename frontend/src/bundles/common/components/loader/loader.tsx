cb1f1f0619a71f989a18af736efcf81a05a256f6
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-05 06:07:54 -0500

    OV-164: * change loader styling to module.css

--- frontend/src/bundles/common/components/loader/loader.tsx
+++ frontend/src/bundles/common/components/loader/loader.tsx
@@ -1,24 +1,17 @@
 import { Box, Circle, Flex, Text } from '@chakra-ui/react';
 
 import { SPIN_ANIMATION } from './libs/constants/constants.js';
+import styles from './loader.module.css';
 
 const Loader = (): JSX.Element => {
     return (
         <Flex flexDirection="column" alignItems="center">
             <Box position="relative" width="100px" height="100px">
-                <Circle
-                    size="full"
-                    backgroundColor="white"
-                    color="text.default"
-                >
+                <Circle className={styles['loaderCircle']}>
                     LOGO
                 </Circle>
                 <Circle
-                    position="absolute"
-                    inset="0"
-                    borderWidth="5px"
-                    borderColor="shadow.200"
-                    borderTopColor="brand.secondary.300"
+                    className={styles['loaderCircleAnimation']}
                     animation={`${SPIN_ANIMATION} 1s linear infinite`}
                 />
             </Box>
