329aec728c41008339e84e22c1762d8bb9347681
JKaypa <jose_kaypa@hotmail.com>
2024-09-05 14:56:10 -0500

    OV-140: - remove unnecesary setter

--- frontend/src/bundles/common/components/sidebar/hooks/use-collapse/use-collapse.hook.ts
+++ frontend/src/bundles/common/components/sidebar/hooks/use-collapse/use-collapse.hook.ts
@@ -33,8 +33,6 @@ const useCollapse = (): {
     };
 
     useEffect(() => {
-        // setIsCollapsed(isSidebarCollapsed);
-
         window.addEventListener('resize', resize);
 
         return () => {
