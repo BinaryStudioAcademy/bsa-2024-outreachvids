040e84abd45cdba66dfc81005cf96416b9760f3d
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-05 15:40:17 -0500

    OV-164: * change menu styling to module.css

--- frontend/src/bundles/studio/components/video-menu/components/menu/menu.tsx
+++ frontend/src/bundles/studio/components/video-menu/components/menu/menu.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, VStack } from '~/bundles/common/components/components.js';
 import { useCallback } from '~/bundles/common/hooks/hooks.js';
 
 import { type MenuItem } from '../../types/types.js';
+import styles from './Menu.module.css';
 
 type Properties = {
     items: MenuItem[];
@@ -32,57 +33,20 @@ const Menu: React.FC<Properties> = ({
         },
         [onActiveIndexSet, items],
     );
+
     return (
-        <Box
-            sx={{
-                position: 'fixed',
-                left: 0,
-                top: '50%',
-                transform: 'translateY(-50%)',
-                h: 'auto',
-                w: '84px',
-                bg: 'background.900',
-                color: 'white',
-                borderTopRightRadius: 'xl',
-                borderBottomRightRadius: 'xl',
-            }}
-        >
-            <VStack spacing={1} align="stretch" p={2} w="100%">
+        <Box className={styles['menuContainer']}>
+            <VStack spacing={1} align="stretch" className={styles['vStack']}>
                 {items.map((item, index) => (
                     <Flex
                         key={index}
                         onClick={handleClick(index)}
-                        sx={{
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            p: 2,
-                            w: '66px',
-                            gap: 1,
-                            cursor: 'pointer',
-                            borderRadius: '8px',
-                            bg:
-                                activeIndex === index
-                                    ? 'background.600'
-                                    : 'transparent',
-                            _hover: {
-                                bg: 'background.600',
-                            },
-                        }}
+                        className={`${styles['menuItem']} ${
+                            activeIndex === index ? styles['menuItemActive'] : ''
+                        }`}
                     >
-                        <Box
-                            sx={{
-                                fontSize: 'xl',
-                            }}
-                        >
-                            {item.icon}
-                        </Box>
-                        <Box
-                            sx={{
-                                fontSize: '12px',
-                            }}
-                        >
-                            {item.label}
-                        </Box>
+                        <Box className={styles['icon']}>{item.icon}</Box>
+                        <Box className={styles['label']}>{item.label}</Box>
                     </Flex>
                 ))}
             </VStack>
