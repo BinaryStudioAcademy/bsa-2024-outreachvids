2f9c1965639072e7c850b1b1d4037e5098ef430a
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-05 15:55:24 -0500

    OV-164: * fix video card styling

--- frontend/src/bundles/home/components/video-card/video-card.tsx
+++ frontend/src/bundles/home/components/video-card/video-card.tsx
@@ -11,23 +11,35 @@ import {
 } from '~/bundles/common/components/components.js';
 import { IconName } from '~/bundles/common/icons/icons.js';
 
-import styles from './video-card.module.css';
-
 const VideoCard: React.FC = () => {
     return (
-        <Box className={styles['container']}>
-            <Box className={styles['imageContainer']} role="group">
-                <Image src={photo} alt="Video preview" className={styles['image']} />
+        <Box borderRadius="8px" bg="white" padding="7px">
+            <Box position="relative" role="group">
+                <Image src={photo} alt="Video preview" borderRadius="5px" />
 
                 {/* Overlay effect */}
                 <Box
-                    className={styles['overlay']}
+                    position="absolute"
+                    top="0"
+                    left="0"
+                    width="100%"
+                    height="100%"
+                    bg="rgba(53, 57, 154, 0.3)"
+                    opacity="0"
+                    transition="opacity 0.3s ease"
                     _groupHover={{ opacity: 1 }}
+                    borderRadius="5px"
                 />
 
                 <IconButton
                     aria-label="Video options"
-                    className={`${styles['iconButton']} ${styles['iconButtonTopRight']}`}
+                    position="absolute"
+                    bg="white"
+                    top="5px"
+                    right="5px"
+                    size="xs"
+                    opacity="0"
+                    transition="opacity 0.3s ease"
                     _groupHover={{ opacity: 1 }}
                     icon={
                         <Icon
@@ -41,7 +53,14 @@ const VideoCard: React.FC = () => {
                 <IconButton
                     aria-label="Edit video"
                     isRound={true}
-                    className={`${styles['iconButton']} ${styles['iconButtonCenter']}`}
+                    size="lg"
+                    position="absolute"
+                    bg="white"
+                    top="50%"
+                    left="calc(50% - 12.5px)"
+                    transform="translate(-50%, -50%)"
+                    opacity="0"
+                    transition="opacity 0.3s ease"
                     _groupHover={{ opacity: 1 }}
                     icon={
                         <Icon
@@ -53,15 +72,15 @@ const VideoCard: React.FC = () => {
                 />
             </Box>
 
-            <Box className={styles['textContainer']}>
-                <Text variant="button" className={styles['textButton']}>
+            <Box padding="7px 10px 5px 5px">
+                <Text variant="button" color="typography.900">
                     Video Name
                 </Text>
                 <Flex justify="space-between">
-                    <Text variant="caption" className={styles['textCaption']}>
+                    <Text variant="caption" color="typography.300">
                         Aug 9, 2024, 1:24 PM
                     </Text>
-                    <Text variant="caption" className={styles['textCaption']}>
+                    <Text variant="caption" color="typography.300">
                         0,30 sec
                     </Text>
                 </Flex>
