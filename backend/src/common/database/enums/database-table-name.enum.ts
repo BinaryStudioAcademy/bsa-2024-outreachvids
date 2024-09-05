0bb6d4bcf50f8618a21c6847d85e2fbd20b2eb3e
XCODE89 <maph0989@gmail.com>
2024-09-05 09:55:30 -0400

    OV-60: * update DatabaseTableName

--- backend/src/common/database/enums/database-table-name.enum.ts
+++ backend/src/common/database/enums/database-table-name.enum.ts
@@ -3,6 +3,11 @@ const DatabaseTableName = {
     USERS: 'users',
     VIDEOS: 'videos',
     FILES: 'files',
+    AVATARS: 'avatars',
+    STYLES: 'styles',
+    GESTURES: 'gestures',
+    AVATARS_STYLES: 'avatars_styles',
+    AVATARS_STYLES_GESTURES: 'avatars_styles_gestures',
 } as const;
 
 export { DatabaseTableName };
