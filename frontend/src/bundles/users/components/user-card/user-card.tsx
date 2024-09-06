b56ec66cb90b9dde21919e74718212ff2d25bb59
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-06 05:20:03 -0500

    OV-139: * prettier

--- frontend/src/bundles/users/components/user-card/user-card.tsx
+++ frontend/src/bundles/users/components/user-card/user-card.tsx
@@ -5,12 +5,11 @@ import {
     VideoModal,
     VStack,
 } from '~/bundles/common/components/components.js';
-import { useCallback,useState } from '~/bundles/common/hooks/hooks.js';
+import { useCallback, useState } from '~/bundles/common/hooks/hooks.js';
 
 import { UserAvatar } from './components/user-avatar.js';
 
 const UserCard: React.FC = () => {
-
     const [isModalOpen, setIsModalOpen] = useState(false);
     const openModal = useCallback((): void => {
         setIsModalOpen(true);
@@ -19,21 +18,26 @@ const UserCard: React.FC = () => {
         setIsModalOpen(false);
     }, []);
     return (
-    <VStack rounded="lg" bg="background.600" spacing="10px" p="15px 5px 10px">
-        <Flex
-            w="full"
-            align="center"
-            color="brand.secondary.900"
-            gap="15px"
-            pl="10px"
+        <VStack
+            rounded="lg"
+            bg="background.600"
+            spacing="10px"
+            p="15px 5px 10px"
         >
-            {/* TODO: replace Circle and Text content with dynamic values */}
-            <UserAvatar username="FN" />
-            <Text>Firstname Lastname</Text>
-        </Flex>
-        <Button label="Create video" onClick={openModal}/>
-        <VideoModal isOpen={isModalOpen} onModalClose={closeModal} />
-    </VStack>
+            <Flex
+                w="full"
+                align="center"
+                color="brand.secondary.900"
+                gap="15px"
+                pl="10px"
+            >
+                {/* TODO: replace Circle and Text content with dynamic values */}
+                <UserAvatar username="FN" />
+                <Text>Firstname Lastname</Text>
+            </Flex>
+            <Button label="Create video" onClick={openModal} />
+            <VideoModal isOpen={isModalOpen} onModalClose={closeModal} />
+        </VStack>
     );
 };
 
