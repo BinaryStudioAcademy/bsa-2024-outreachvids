c488380a30dbcb588991fcae0640f306a0537341
JKaypa <jose_kaypa@hotmail.com>
2024-09-06 11:52:07 -0500

    OV-187: * fix loadcurrentuser call

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,13 +1,18 @@
 import { actions as authActions } from '~/bundles/auth/store/auth.js';
 import { RouterOutlet } from '~/bundles/common/components/components.js';
 import { useAppDispatch, useEffect } from '~/bundles/common/hooks/hooks.js';
+import { storage, StorageKey } from '~/framework/storage/storage.js';
 
 const App: React.FC = () => {
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        void dispatch(authActions.loadCurrentUser());
-    });
+        void storage.has(StorageKey.TOKEN).then((hasToken) => {
+            if (hasToken) {
+                void dispatch(authActions.loadCurrentUser());
+            }
+        });
+    }, [dispatch]);
     return (
         <>
             <RouterOutlet />
