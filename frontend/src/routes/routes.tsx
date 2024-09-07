d5be4bfdaf12a5f3b43cd27a61ae4c8bfefa207e
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-07 12:00:36 +0200

    OV-194: * remove space at the bottom of the page

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -23,7 +23,7 @@ const routes = [
                 path: AppRoute.ROOT,
                 element: (
                     <ProtectedRoute>
-                        <Home />,
+                        <Home />
                     </ProtectedRoute>
                 ),
             },
@@ -31,7 +31,7 @@ const routes = [
                 path: AppRoute.STUDIO,
                 element: (
                     <ProtectedRoute>
-                        <Studio />,
+                        <Studio />
                     </ProtectedRoute>
                 ),
             },
@@ -39,7 +39,7 @@ const routes = [
                 path: AppRoute.MY_AVATAR,
                 element: (
                     <ProtectedRoute>
-                        <MyAvatar />,
+                        <MyAvatar />
                     </ProtectedRoute>
                 ),
             },
