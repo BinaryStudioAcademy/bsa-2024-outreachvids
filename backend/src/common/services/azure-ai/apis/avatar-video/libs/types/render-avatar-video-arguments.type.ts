b59bf3d4670227762db06117cf6c8d275eb1fdf4
Sergiy <sergiy0378@gmail.com>
2024-09-07 16:57:09 +0300

    OV-196: + libs

---
+++ backend/src/common/services/azure-ai/apis/avatar-video/libs/types/render-avatar-video-arguments.type.ts
@@ -0,0 +1,8 @@
+import { type RenderAvatarVideoRequestDto } from '~/common/services/azure-ai/types/types.js';
+
+type RenderAvatarVideoArgument = {
+    id: string;
+    payload: RenderAvatarVideoRequestDto;
+};
+
+export { type RenderAvatarVideoArgument };
---
+++ backend/src/common/services/azure-ai/apis/avatar-video/libs/types/types.ts
@@ -0,0 +1,1 @@
+export { type RenderAvatarVideoArgument } from './render-avatar-video-arguments.type.js';
