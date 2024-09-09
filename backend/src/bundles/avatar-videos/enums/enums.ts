dd574535d23819a659b2c11d463a39576b105f03
Sergiy <sergiy0378@gmail.com>
2024-09-09 14:17:42 +0300

    OV-173: + enum

--- backend/src/bundles/avatar-videos/enums/enums.ts
+++ backend/src/bundles/avatar-videos/enums/enums.ts
@@ -1,1 +1,3 @@
+export { GenerateAvatarResponseStatus } from './generate-avatar-response-status.enum.js';
+export { RenderVideoErrorMessage } from './render-video-error-message.enum.js';
 export { AvatarVideosApiPath } from 'shared';
---
+++ backend/src/bundles/avatar-videos/enums/render-video-error-message.enum.ts
@@ -0,0 +1,5 @@
+const RenderVideoErrorMessage = {
+    NOT_FOUND: 'Video not found',
+} as const;
+
+export { RenderVideoErrorMessage };
