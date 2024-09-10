504cac9f238617c27a46aa402029ab9bc79ab2b3
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-10 16:06:51 +0200

    OV-198: * add button to scenes row

--- frontend/src/bundles/studio/components/timeline/rows/scenes-row.tsx
+++ frontend/src/bundles/studio/components/timeline/rows/scenes-row.tsx
@@ -1,7 +1,15 @@
-import { Badge, Image } from '~/bundles/common/components/components.js';
+import {
+    Badge,
+    Icon,
+    IconButton,
+    Image,
+    Tooltip,
+} from '~/bundles/common/components/components.js';
 import { useAppSelector, useMemo } from '~/bundles/common/hooks/hooks.js';
+import { IconName, IconSize } from '~/bundles/common/icons/icons.js';
 import { RowNames } from '~/bundles/studio/enums/enums.js';
 import { setItemsSpan } from '~/bundles/studio/helpers/set-items-span.js';
+import { useTimelineContext } from '~/bundles/studio/hooks/hooks.js';
 import styles from '~/framework/theme/styles/css-modules/timeline.module.css';
 
 import { Item, Row } from '../components.js';
@@ -9,6 +17,13 @@ import { Item, Row } from '../components.js';
 const ScenesRow: React.FC = () => {
     const scenes = useAppSelector(({ studio }) => studio.scenes);
     const scenesWithSpan = useMemo(() => setItemsSpan(scenes), [scenes]);
+    const { pixelsToValue } = useTimelineContext();
+
+    const scenesEnd = scenesWithSpan.at(-1)?.span.end ?? 0;
+    const buttonWidthInPixels = 60;
+    const buttonWidth = pixelsToValue(buttonWidthInPixels);
+    const buttonEnd =
+        scenesEnd + (Number.isFinite(buttonWidth) ? buttonWidth : 0);
 
     return (
         <Row
@@ -35,6 +50,22 @@ const ScenesRow: React.FC = () => {
                     )}
                 </Item>
             ))}
+
+            <Item
+                type={RowNames.SCENE}
+                id="Add scene button"
+                span={{ start: scenesEnd, end: buttonEnd }}
+            >
+                <Tooltip hasArrow label={'Add a scene'} placement="top">
+                    <IconButton
+                        height="100%"
+                        width="100%"
+                        size={IconSize.LARGE}
+                        aria-label={'Add a scene'}
+                        icon={<Icon as={IconName.ADD} />}
+                    />
+                </Tooltip>
+            </Item>
         </Row>
     );
 };
