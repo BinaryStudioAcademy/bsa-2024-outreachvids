2d9f332fb083bee508302c7d0eb018c7a8150a2a
Luis Felix <lfelix3011@gmail.com>
2024-09-10 11:37:31 -0400

    OV-42: * adjust in styles format and use of general loader

--- frontend/src/bundles/chat/components/message-box/message-box.tsx
+++ frontend/src/bundles/chat/components/message-box/message-box.tsx
@@ -2,7 +2,7 @@ import { MessageSender } from '~/bundles/chat/enums/enums.js';
 import { type Message } from '~/bundles/chat/types/types.js';
 import { Box, Flex, Text } from '~/bundles/common/components/components.js';
 
-import styles from './message-box.module.css';
+import styles from './styles.module.css';
 
 type Properties = {
     message: Message;
@@ -18,12 +18,7 @@ const MessageBox: React.FC<Properties> = ({ message }) => {
             <Flex
                 className={`${styles['message-box']} ${styles[sender === MessageSender.USER ? 'message-box--user' : 'message-box--ai']}`}
             >
-                <Text
-                    color={'black'}
-                    bg={'background.50'}
-                    p={3}
-                    borderRadius="md"
-                >
+                <Text color="black" bg="background.50" p={3} borderRadius="md">
                     {text}
                 </Text>
             </Flex>
