da4245cd59913a03782d298a51bfab02b991066a
JKaypa <jose_kaypa@hotmail.com>
2024-09-10 23:43:49 -0500

    OV-204: + add loader and dispatch

--- frontend/src/bundles/studio/components/video-menu/components/menu-content/script-content/script-content.tsx
+++ frontend/src/bundles/studio/components/video-menu/components/menu-content/script-content/script-content.tsx
@@ -1,13 +1,16 @@
 import {
     Icon,
     IconButton,
+    Loader,
     Text,
     VStack,
 } from '~/bundles/common/components/components.js';
+import { DataStatus } from '~/bundles/common/enums/data-status.enum.js';
 import {
     useAppDispatch,
     useAppSelector,
     useCallback,
+    useEffect,
 } from '~/bundles/common/hooks/hooks.js';
 import { IconName } from '~/bundles/common/icons/icons.js';
 import { actions as studioActions } from '~/bundles/studio/store/studio.js';
@@ -17,7 +20,11 @@ import { NEW_SCRIPT_TEXT } from './constants/constants.js';
 
 const ScriptContent: React.FC = () => {
     const dispatch = useAppDispatch();
-    const scripts = useAppSelector(({ studio }) => studio.scripts);
+    const { scripts, voices } = useAppSelector(({ studio }) => studio);
+
+    useEffect(() => {
+        void dispatch(studioActions.loadVoices());
+    }, [dispatch]);
 
     const handleAddScript = useCallback((): void => {
         void dispatch(studioActions.addScript(NEW_SCRIPT_TEXT));
@@ -25,21 +32,27 @@ const ScriptContent: React.FC = () => {
 
     return (
         <VStack w="full" spacing="20px" p="20px 0">
-            {scripts.length === 0 ? (
-                <Text variant="body1" width="60%" textAlign="center">
-                    To add a script press a button below.
-                </Text>
+            {voices.dataStatus === DataStatus.PENDING ? (
+                <Loader />
             ) : (
-                scripts.map(({ id, ...script }) => (
-                    <Script key={id} id={id} {...script} />
-                ))
+                <>
+                    {scripts.length === 0 ? (
+                        <Text variant="body1" width="60%" textAlign="center">
+                            To add a script press a button below.
+                        </Text>
+                    ) : (
+                        scripts.map(({ id, ...script }) => (
+                            <Script key={id} id={id} {...script} />
+                        ))
+                    )}
+                    <IconButton
+                        icon={<Icon as={IconName.ADD} />}
+                        aria-label="Add script"
+                        borderRadius="100%"
+                        onClick={handleAddScript}
+                    />
+                </>
             )}
-            <IconButton
-                icon={<Icon as={IconName.ADD} />}
-                aria-label="Add script"
-                borderRadius="100%"
-                onClick={handleAddScript}
-            />
         </VStack>
     );
 };
