6b60afa7751848ef1a1cee1f19f5d0b8d199b278
JKaypa <jose_kaypa@hotmail.com>
2024-09-10 23:28:39 -0500

    OV-204: + export voice type

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -15,6 +15,7 @@ export {
     type GenerateSpeechRequestDto,
     type GenerateSpeechResponseDto,
     type GetVoicesResponseDto,
+    type Voice,
     generateSpeechValidationSchema,
     SpeechApiPath,
 } from './bundles/speech/speech.js';
