855d5e8635dab87f1d9422eaea91fcda9a00447d
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-11 18:31:11 +0200

    OV-257: * add load user videos action to all actions

--- frontend/src/bundles/home/store/home.ts
+++ frontend/src/bundles/home/store/home.ts
@@ -1,7 +1,9 @@
+import { loadUserVideos } from './actions.js';
 import { actions } from './slice.js';
 
 const allActions = {
     ...actions,
+    loadUserVideos,
 };
 
 export { reducer } from './slice.js';
