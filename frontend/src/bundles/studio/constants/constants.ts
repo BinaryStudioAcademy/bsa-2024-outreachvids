ce33dcad838e8d188d26b839e48e9dd0368c0081
Sergiy <sergiy0378@gmail.com>
2024-09-12 00:57:14 +0300

    OV-254: + constants

--- frontend/src/bundles/studio/constants/constants.ts
+++ frontend/src/bundles/studio/constants/constants.ts
@@ -1,5 +1,6 @@
 export { MIN_SCENE_DURATION } from './min-scene-duration.constant.js';
 export { MIN_SCRIPT_DURATION } from './min-script-duration.constant.js';
+export { SCRIPT_AND_AVATAR_ARE_REQUIRED } from './script-and-avatar-are-required.constant.js';
 export { SKIP_TO_PREV_SCENE_THRESHOLD } from './skip-to-previous-scene-threshold.constant.js';
 export { VIDEO_SUBMIT_FAILED_NOTIFICATION_ID } from './video-submit-failed-id.constant.js';
 export { VIDEO_SUBMIT_NOTIFICATION_ID } from './video-submit-notification-id.constant.js';
