082f86baae10391c73488e3f8c7d6dd6f8097da9
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-11 06:30:35 -0500

    OV-232: * use T type and remove magic numbers

--- frontend/src/bundles/studio/helpers/get-total-time.ts
+++ frontend/src/bundles/studio/helpers/get-total-time.ts
@@ -1,11 +1,11 @@
-type HasDuration = { duration: number };
+import { secondsToMilliseconds } from 'date-fns';
 
-const calculateTotalMilliseconds = (
-    items: HasDuration[],
+const calculateTotalMilliseconds = <T extends { duration: number }>(
+    items: T[],
     rangeEnd: number,
 ): number => {
     const totalSeconds = items.reduce((sum, item) => sum + item.duration, 0);
-    const totalMilliseconds = totalSeconds * 1000;
+    const totalMilliseconds = secondsToMilliseconds(totalSeconds);
     return totalMilliseconds > rangeEnd ? totalMilliseconds : rangeEnd;
 };
 
