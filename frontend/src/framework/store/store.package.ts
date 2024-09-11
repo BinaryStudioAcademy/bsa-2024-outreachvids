b278673ab13b35306d31e2a1f766173ffe3f6384
Sergiy <sergiy0378@gmail.com>
2024-09-11 16:25:57 +0300

    OV-254: + avatar-videos-api

--- frontend/src/framework/store/store.package.ts
+++ frontend/src/framework/store/store.package.ts
@@ -9,7 +9,7 @@ import { authApi } from '~/bundles/auth/auth.js';
 import { reducer as authReducer } from '~/bundles/auth/store/auth.js';
 import { AppEnvironment } from '~/bundles/common/enums/enums.js';
 import { reducer as studioReducer } from '~/bundles/studio/store/studio.js';
-import { avatarsApi } from '~/bundles/studio/studio.js';
+import { avatarsApi, avatarVideosApi } from '~/bundles/studio/studio.js';
 import { userApi } from '~/bundles/users/users.js';
 import { type Config } from '~/framework/config/config.js';
 import { storage } from '~/framework/storage/storage.js';
@@ -25,6 +25,7 @@ type ExtraArguments = {
     authApi: typeof authApi;
     userApi: typeof userApi;
     avatarsApi: typeof avatarsApi;
+    avatarVideosApi: typeof avatarVideosApi;
     storage: typeof storage;
 };
 
@@ -60,6 +61,7 @@ class Store {
             authApi,
             userApi,
             avatarsApi,
+            avatarVideosApi,
             storage,
         };
     }
