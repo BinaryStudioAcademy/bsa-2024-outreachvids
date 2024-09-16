d7e7bcefae5d0066687a07982b6eb60a79badce5
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-09-16 14:38:57 +0300

    OV-308: * fix modal usage

--- frontend/src/bundles/common/components/modal/modal.tsx
+++ frontend/src/bundles/common/components/modal/modal.tsx
@@ -9,13 +9,13 @@ import { type ModalProps } from '~/bundles/common/types/types.js';
 
 import styles from './styles.module.css';
 
-const Modal: React.FC<ModalProps> = (properties) => {
+const Modal: React.FC<ModalProps> = ({ children, ...properties }) => {
     return (
         <LibraryModal {...properties} isCentered>
             <ModalOverlay />
             <ModalContent className={styles['modal-content']}>
                 <ModalCloseButton margin="20px" zIndex={10} />
-                <ModalBody padding={0}>{properties.children}</ModalBody>
+                <ModalBody padding={0}>{children}</ModalBody>
             </ModalContent>
         </LibraryModal>
     );
