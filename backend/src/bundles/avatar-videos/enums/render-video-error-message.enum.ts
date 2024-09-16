4bd5a698f074fd7daf8ce6fd53e50f2d2a230d23
Sergiy <sergiy0378@gmail.com>
2024-09-16 21:45:08 +0300

    OV-298: * enum

--- backend/src/bundles/avatar-videos/enums/render-video-error-message.enum.ts
+++ backend/src/bundles/avatar-videos/enums/render-video-error-message.enum.ts
@@ -1,6 +1,7 @@
 const RenderVideoErrorMessage = {
     NOT_FOUND: 'Video not found',
     NOT_SAVED: 'Video was not saved',
+    RENDER_ERROR: 'Avatar rendering error',
 } as const;
 
 export { RenderVideoErrorMessage };
