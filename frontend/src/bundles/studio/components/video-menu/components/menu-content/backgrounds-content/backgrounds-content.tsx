210fdebf4d389f82e32e4a3f33a3317353d4da52
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-19 16:06:16 +0200

    OV-358: * add layout for bg images

--- frontend/src/bundles/studio/components/video-menu/components/menu-content/backgrounds-content/backgrounds-content.tsx
+++ frontend/src/bundles/studio/components/video-menu/components/menu-content/backgrounds-content/backgrounds-content.tsx
@@ -1,5 +1,33 @@
+import {
+    Box,
+    Image,
+    SimpleGrid,
+    Text,
+} from '~/bundles/common/components/components.js';
+
+import { backgroundImages } from '../../mock/bg-images-mock.js';
+import styles from './styles.module.css';
+
 const BackgroundsContent: React.FC = () => {
-    return <div>Backgrounds</div>;
+    return (
+        <>
+            <Text variant="body1" mb="12px">
+                Images
+            </Text>
+            <SimpleGrid columns={2} spacingX="13px" spacingY="10px">
+                {backgroundImages.map((imageSource, index) => (
+                    <Box key={index} className={styles['image-item']}>
+                        <Image
+                            src={imageSource}
+                            objectFit="contain"
+                            width="100%"
+                            height="100%"
+                        ></Image>
+                    </Box>
+                ))}
+            </SimpleGrid>
+        </>
+    );
 };
 
 export { BackgroundsContent };
