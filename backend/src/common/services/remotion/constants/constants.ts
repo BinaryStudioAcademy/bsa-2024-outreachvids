6dead9f94fc481d3e9ae2b5bfc7827bc9b80accd
Sergiy <sergiy0378@gmail.com>
2024-09-21 00:20:53 +0300

    OV-378: + constants

--- backend/src/common/services/remotion/constants/constants.ts
+++ backend/src/common/services/remotion/constants/constants.ts
@@ -1,3 +1,5 @@
 export { CODEC } from './codec.constant.js';
 export { COMPOSITION_ID } from './composition-id.constant.js';
+export { DOWNLOAD_BEHAVIOR } from './download-behavior.constant.js';
+export { DOWNLOADED_FILE_NAME } from './downloaded-file-name.constant.js';
 export { REQUEST_DELAY } from './request-delay.constant.js';
