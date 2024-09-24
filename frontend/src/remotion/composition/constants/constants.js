22866ac8a1307c41be2c5aa2241934bcb9884469
Sergiy <sergiy0378@gmail.com>
2024-09-24 20:08:51 +0300

    OV-396: + constants

---
+++ frontend/src/remotion/composition/constants/constants.js
@@ -0,0 +1,7 @@
+// Using .js extension to avoid TypeScript issues with Remotion's rendering process.
+export { FPS } from './fps.constant.js';
+export { LANDSCAPE_HEIGHT } from './height-landscape.constant.js';
+export { PORTRAIT_HEIGHT } from './height-portrait.js';
+export { MINIMAL_DURATION_IN_FRAMES } from './minimal-duration-in-frames.constant.js';
+export { LANDSCAPE_WIDTH } from './width-landscape.constant.js';
+export { PORTRAIT_WIDTH } from './width-portrait.constant.js';
