4ad8717e96e61faae56520bb63eb155b02df4353
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-09-24 18:56:08 +0200

    OV-418: * add `none` buttons to background choices

--- frontend/src/bundles/studio/components/video-menu/components/menu-content/backgrounds-content/backgrounds-content.tsx
+++ frontend/src/bundles/studio/components/video-menu/components/menu-content/backgrounds-content/backgrounds-content.tsx
@@ -1,16 +1,19 @@
 import {
+    Flex,
     SimpleGrid,
     Tab,
     TabList,
     TabPanel,
     TabPanels,
     Tabs,
+    Text,
 } from '~/bundles/common/components/components.js';
 // TODO: Make endpoint for this
 import backgroundColors from '~/bundles/studio/data/bg-colors.json';
 import backgroundImages from '~/bundles/studio/data/bg-images.json';
 
 import { ColorCard, ImageCard } from './components/components.js';
+import styles from './styles.module.css';
 
 const BackgroundsContent: React.FC = () => {
     return (
@@ -24,6 +27,13 @@ const BackgroundsContent: React.FC = () => {
                 <TabPanels>
                     <TabPanel>
                         <SimpleGrid columns={2} spacingX="13px" spacingY="10px">
+                            <Flex
+                                className={styles['none-item']}
+                                height="100px"
+                            >
+                                <Text variant="body1">None</Text>
+                            </Flex>
+
                             {backgroundImages.map((imageSource, index) => (
                                 <ImageCard
                                     key={index}
@@ -34,6 +44,10 @@ const BackgroundsContent: React.FC = () => {
                     </TabPanel>
                     <TabPanel>
                         <SimpleGrid columns={3} spacingX="13px" spacingY="10px">
+                            <Flex className={styles['none-item']} height="80px">
+                                <Text variant="body1">None</Text>
+                            </Flex>
+
                             {backgroundColors.map((color, index) => (
                                 <ColorCard key={index} color={color} />
                             ))}
