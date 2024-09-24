702097529ee3ebe355f597cddfeb80ab47afd037
Sergiy <sergiy0378@gmail.com>
2024-09-24 20:09:10 +0300

    OV-396: + enums

---
+++ frontend/src/remotion/composition/enums/video-orientation.enum.js
@@ -0,0 +1,7 @@
+// Using .js extension to avoid TypeScript issues with Remotion's rendering process.
+const videoOrientation = {
+    LANDSCAPE: 'landscape',
+    PORTRAIT: 'portrait',
+};
+
+export { videoOrientation };
