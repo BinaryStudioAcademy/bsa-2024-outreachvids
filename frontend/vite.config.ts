69ab354e2843c171f45e653d2fbd8214b3e02c9b
Sergiy <sergiy0378@gmail.com>
2024-09-25 17:06:26 +0300

    OV-426: * vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
         VITE_APP_DEVELOPMENT_PORT,
         VITE_APP_API_ORIGIN_URL,
         VITE_APP_PROXY_SERVER_URL,
+        VITE_APP_SOCKET_ORIGIN_URL,
     } = loadEnv(mode, process.cwd());
 
     return defineConfig({
@@ -53,6 +54,10 @@ const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
             port: Number(VITE_APP_DEVELOPMENT_PORT),
             proxy: {
                 [VITE_APP_API_ORIGIN_URL as string]: {
+                    target: VITE_APP_PROXY_SERVER_URL,
+                    changeOrigin: true,
+                },
+                [VITE_APP_SOCKET_ORIGIN_URL as string]: {
                     target: VITE_APP_PROXY_SERVER_URL,
                     changeOrigin: true,
                     ws: true,
