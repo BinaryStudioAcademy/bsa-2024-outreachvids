556ed1d693149729abb2f8511bbc251a20bff0ac
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-26 09:01:15 -0500

    OV-377: - use config to create video url

--- frontend/src/bundles/home/helpers/create-url-token.ts
+++ frontend/src/bundles/home/helpers/create-url-token.ts
@@ -1,7 +1,10 @@
+import { AppEnvironment } from '~/bundles/common/enums/enums.js';
+import { config } from '~/framework/config/config.js';
+
 const createVideoUrl = (jwtToken: string): string => {
     const baseUrl =
-        import.meta.env['VITE_APP_NODE_ENV'] === 'production'
-            ? 'http://bsa-2024-outreachvids-dev.eu-north-1.elasticbeanstalk.com/'
+        config.ENV.APP.ENVIRONMENT === AppEnvironment.PRODUCTION
+            ? 'http://bsa-2024-outreachvids-dev.eu-north-1.elasticbeanstalk.com'
             : 'http://localhost:3000';
 
     return `${baseUrl}/preview/${jwtToken}`;
