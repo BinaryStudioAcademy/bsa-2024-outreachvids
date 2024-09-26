480e38f75dd1450008aa0754be904c08f5733ab3
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-09-26 08:40:45 -0500

    OV-377: + added dynamically created sharing URL

--- frontend/src/bundles/home/helpers/create-url-token.ts
+++ frontend/src/bundles/home/helpers/create-url-token.ts
@@ -1,5 +1,9 @@
 const createVideoUrl = (jwtToken: string): string => {
-    const baseUrl = 'http://localhost:3000';
+    const baseUrl =
+        import.meta.env['VITE_APP_NODE_ENV'] === 'production'
+            ? 'http://bsa-2024-outreachvids-dev.eu-north-1.elasticbeanstalk.com/'
+            : 'http://localhost:3000';
+
     return `${baseUrl}/preview/${jwtToken}`;
 };
 
