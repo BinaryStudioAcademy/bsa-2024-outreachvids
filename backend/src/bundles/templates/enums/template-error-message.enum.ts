9bd6ded3a6f29081b1aff327e1d40b0f621ef64d
Sergiy <sergiy0378@gmail.com>
2024-09-26 14:21:18 +0300

    OV-404: * enum

--- backend/src/bundles/templates/enums/template-error-message.enum.ts
+++ backend/src/bundles/templates/enums/template-error-message.enum.ts
@@ -1,5 +1,6 @@
 const templateErrorMessage = {
     YOU_CAN_NOT_DELETE_THIS_TEMPLATE: 'You can not delete this template',
+    YOU_CAN_NOT_UPDATE_THIS_TEMPLATE: 'You can not update this template',
     TEMPLATE_DOES_NOT_EXIST: 'Template does not exist',
 } as const;
 
