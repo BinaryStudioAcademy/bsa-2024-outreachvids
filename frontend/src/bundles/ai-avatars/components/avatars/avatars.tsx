c1f322f3c6960caf03fca7d59f3d5acce90dbb0e
JKaypa <jose_kaypa@hotmail.com>
2024-09-26 20:41:23 -0500

    OV-402: + filters logic

--- frontend/src/bundles/ai-avatars/components/avatars/avatars.tsx
+++ frontend/src/bundles/ai-avatars/components/avatars/avatars.tsx
@@ -1,8 +1,42 @@
-import { Box, Text } from '~/bundles/common/components/components.js';
+import { EMPTY_LENGTH } from '~/bundles/ai-avatars/constants/constants.js';
+import { avatarsMapper } from '~/bundles/ai-avatars/helpers/helpers.js';
+import { useFilterAvatarStyle } from '~/bundles/ai-avatars/hooks/use-filter-avatar-style.hook.js';
+import { Box, Heading } from '~/bundles/common/components/components.js';
+import {
+    useAppDispatch,
+    useAppForm,
+    useAppSelector,
+    useEffect,
+    useMemo,
+} from '~/bundles/common/hooks/hooks.js';
+import { actions as studioActions } from '~/bundles/studio/store/studio.js';
 
-import { AvatarsLibrary } from '../components.js';
+import { AvatarsSection } from '../components.js';
 
 const Avatars: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const { avatars } = useAppSelector(({ studio }) => studio);
+
+    const form = useAppForm<{ style: string }>({
+        initialValues: { style: '' },
+    });
+    const {
+        values: { style },
+    } = form;
+
+    const styledAvatars = useFilterAvatarStyle(style);
+    const mappedAvatars = useMemo(() => avatarsMapper(avatars), [avatars]);
+    const favoriteAvatars = useMemo(
+        () => mappedAvatars.filter(({ isLiked }) => isLiked),
+        [mappedAvatars],
+    );
+
+    useEffect(() => {
+        if (avatars.length === EMPTY_LENGTH) {
+            void dispatch(studioActions.loadAvatars());
+        }
+    }, [dispatch, avatars.length]);
+
     return (
         <Box bg="background.900" pr="25px">
             <Box
@@ -11,8 +45,18 @@ const Avatars: React.FC = () => {
                 p="25px"
                 minH="calc(100vh - 75px)"
             >
-                <Text variant="title">AI Avatars</Text>
-                <AvatarsLibrary />
+                <Heading color="typography.900" variant="H3">
+                    AI Avatars
+                </Heading>
+                <AvatarsSection
+                    avatars={favoriteAvatars}
+                    subtitle="My Avatars"
+                />
+                <AvatarsSection
+                    avatars={styledAvatars}
+                    subtitle="OutreachVids Library"
+                    form={form}
+                />
             </Box>
         </Box>
     );
