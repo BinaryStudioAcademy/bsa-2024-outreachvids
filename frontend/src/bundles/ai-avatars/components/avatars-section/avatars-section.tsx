89f4f5c6eac4818e59bc1621c840b2516c5631da
JKaypa <jose_kaypa@hotmail.com>
2024-09-26 20:46:43 -0500

    OV-402: + conditional rendering

---
+++ frontend/src/bundles/ai-avatars/components/avatars-section/avatars-section.tsx
@@ -0,0 +1,86 @@
+import { type FormikProps } from 'formik';
+
+import { EMPTY_LENGTH } from '~/bundles/ai-avatars/constants/constants.js';
+import { type AvatarMapped } from '~/bundles/ai-avatars/types/types.js';
+import {
+    Badge,
+    Box,
+    Flex,
+    FormProvider,
+    Heading,
+    Select,
+    SimpleGrid,
+    Text,
+} from '~/bundles/common/components/components.js';
+
+import { AvatarCard } from '../components.js';
+
+type Properties = {
+    subtitle: string;
+    avatars: AvatarMapped[];
+    form?: FormikProps<{ style: string }>;
+};
+
+const AvatarsSection: React.FC<Properties> = ({ subtitle, avatars, form }) => {
+    return (
+        <Box p="10px 0">
+            <Flex justify="space-between" align="center" mb="10px">
+                <Flex>
+                    <Heading
+                        color="typography.900"
+                        variant="H4"
+                        marginRight="11px"
+                    >
+                        {subtitle}
+                    </Heading>
+                    <Badge
+                        color="background.600"
+                        bg="#D1D4DB"
+                        fontWeight="400"
+                        padding="2px 10px"
+                    >
+                        {avatars.length}
+                    </Badge>
+                </Flex>
+                {form && (
+                    <FormProvider value={form}>
+                        <Box>
+                            <Select name="style" placeholder="All styles">
+                                <option value="business">Business</option>
+                                <option value="casual">Casual</option>
+                                <option value="formal">Formal</option>
+                                <option value="youthful">Youthful</option>
+                                <option value="graceful">Graceful</option>
+                                <option value="technical">Technical</option>
+                            </Select>
+                        </Box>
+                    </FormProvider>
+                )}
+            </Flex>
+            {avatars.length === EMPTY_LENGTH && !form ? (
+                <Text color="typography.600" variant="body1">
+                    Pick your favorites avatars to show them here!
+                </Text>
+            ) : (
+                <SimpleGrid
+                    minChildWidth="300px"
+                    justifyItems="center"
+                    spacing="40px"
+                >
+                    {avatars.map(({ id, imgUrl, name, style, isLiked }) => (
+                        <AvatarCard
+                            key={`${id}-${style}`}
+                            id={id}
+                            name={name}
+                            tag={style}
+                            image={imgUrl}
+                            isLiked={isLiked}
+                        />
+                    ))}
+                </SimpleGrid>
+            )}
+        </Box>
+    );
+};
+
+export { AvatarsSection };
